abb0c4cee64c6084425303a4e5c18603a5cdba51
Iuri Matias <iuri.matias@gmail.com>
2020-05-11 18:23:27 -0400

    attempting to call different proc inside signal handler

--- src/nim_status_client.nim
+++ src/nim_status_client.nim
@@ -19,7 +19,7 @@ proc mainProc() =
   var engine = newQQmlApplicationEngine()
   defer: engine.delete()
 
-  status.setSignalHandler()
+  status.init()
 
   status.setupNewAccount()
   discard status.addPeer("enode://2c8de3cbb27a3d30cbb5b3e003bc722b126f5aef82e2052aaef032ca94e0c7ad219e533ba88c70585ebd802de206693255335b100307645ab5170e88620d2a81@47.244.221.14:443")
