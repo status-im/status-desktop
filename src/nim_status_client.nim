03b6209f6b8807d12913dee4aa8ce07eab96151f
Iuri Matias <iuri.matias@gmail.com>
2020-05-11 13:31:07 -0400

    move status specific logic of the application view

--- src/nim_status_client.nim
+++ src/nim_status_client.nim
@@ -2,6 +2,18 @@ import NimQml
 import applicationLogic
 import chats
 import state
+import status
+import libstatus
+import json
+
+var signalHandler: SignalCallback = proc(p0: cstring): void =
+  setupForeignThreadGc()
+
+  var jsonSignal = ($p0).parseJson
+  if $jsonSignal["type"].getStr == "messages.new":
+    echo $p0
+
+  tearDownForeignThreadGc()
 
 proc mainProc() =
 
@@ -18,6 +30,17 @@ proc mainProc() =
 
   var engine = newQQmlApplicationEngine()
   defer: engine.delete()
+
+
+  status.setSignalHandler(signalHandler)
+
+  status.setupNewAccount()
+  discard status.addPeer("enode://2c8de3cbb27a3d30cbb5b3e003bc722b126f5aef82e2052aaef032ca94e0c7ad219e533ba88c70585ebd802de206693255335b100307645ab5170e88620d2a81@47.244.221.14:443")
+  echo status.callPrivateRPC("{\"jsonrpc\":\"2.0\", \"method\":\"wakuext_requestMessages\", \"params\":[{\"topics\": [\"0x7998f3c8\"]}], \"id\": 1}")
+
+  # result.accountResult = status.queryAccounts()
+  status.subscribeToTest()
+
 
 
   let logic = newApplicationLogic(app)
