18db5dc1451045461f961e48611ce69042a9c121
Iuri Matias <iuri.matias@gmail.com>
2020-05-13 19:02:23 -0400

    add various methods needed for wallet: get price, balance, convert hex wei to eth

---
+++ src/status/wallet.nim
@@ -0,0 +1,33 @@
+import libstatus
+import json
+import utils
+import httpclient, json
+import strformat
+import stint
+
+proc getPrice*(crypto: string, fiat: string): string =
+    var url: string = fmt"https://min-api.cryptocompare.com/data/price?fsym={crypto}&tsyms={fiat}"
+    let client = newHttpClient()
+    client.headers = newHttpHeaders({ "Content-Type": "application/json" })
+
+    let response = client.request(url)
+    $parseJson(response.body)["USD"]
+
+proc getBalance*(address: string): string =
+  let payload = %* {
+    "jsonrpc": "2.0",
+    "id": 50,
+    "method": "eth_getBalance",
+    "params": [
+        address,
+        "latest"
+    ]
+  }
+  parseJson($libstatus.callPrivateRPC($payload))["result"].str
+
+proc hex2Eth*(input: string): string =
+  var value = fromHex(Stuint[256], input)
+  var one_eth = fromHex(Stuint[256], "DE0B6B3A7640000")
+
+  var (eth, remainder) = divmod(value, one_eth)
+  fmt"{eth}.{remainder}"
