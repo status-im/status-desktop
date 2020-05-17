660ebdfdd3342aecbea13106ce907c9ee0159fb5
Richard Ramos <info@richardramos.me>
2020-05-16 21:27:26 -0400

    feat: add signal handler for status-go

--- vendor/DOtherSide/lib/include/DOtherSide/DOtherSide.h
+++ vendor/DOtherSide/lib/include/DOtherSide/DOtherSide.h
@@ -90,6 +90,10 @@ DOS_API void DOS_CALL dos_qapplication_create(void);
 /// \note A QApplication should have been already created through dos_qapplication_create()
 DOS_API void DOS_CALL dos_qapplication_exec(void);
 
+/// \brief Invokes a QObject's slot by passing a string containing a signal
+/// \note This method was created because status-go has a non-QT event loop
+DOS_API void DOS_CALL dos_signal(DosQObject *vptr, const char *signal, const char *slot);
+
 /// \brief Calls the QApplication::quit() function of the current QGuiApplication
 /// \note A QApplication should have been already created through dos_qapplication_create()
 DOS_API void DOS_CALL dos_qapplication_quit(void);
