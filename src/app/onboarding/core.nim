c473ce301205f32275aac519a1ab5b1bfb8484d7
Iuri Matias <iuri.matias@gmail.com>
2020-05-20 13:36:44 -0400

    add controller for onboarding

---
+++ src/app/onboarding/core.nim
@@ -0,0 +1,28 @@
+import NimQml
+# import "../../status/core" as status
+import ../signals/types
+import eventemitter
+import onboarding
+
+type OnboardingController* = ref object of SignalSubscriber
+#   view*: OnboardingView
+  view*: Onboarding
+  variant*: QVariant
+
+proc newController*(events: EventEmitter): OnboardingController =
+  result = OnboardingController()
+#   result.view = newOnboardingView()
+  result.view = newOnboarding(events)
+  result.variant = newQVariant(result.view)
+
+proc delete*(self: OnboardingController) =
+  delete self.view
+  delete self.variant
+
+proc init*(self: OnboardingController) =
+  discard
+
+# method onSignal(self: OnboardingController, data: Signal) =
+#   echo "new signal received"
+#   var msg = cast[WalletSignal](data)
+#   self.view.setLastMessage(msg.content)
