efc8d532201a61628329f59adfbec1ea6e3dee1c
Jonathan Rainville <rainville.jonathan@gmail.com>
2020-06-18 11:24:44 -0400

    feat: add id to the collectible

--- src/app/wallet/views/collectibles_list.nim
+++ src/app/wallet/views/collectibles_list.nim
@@ -6,6 +6,7 @@ type
   CollectiblesRoles {.pure.} = enum
     Name = UserRole + 1,
     Image = UserRole + 2
+    CollectibleId = UserRole + 3
 
 QtObject:
   type CollectiblesList* = ref object of QAbstractListModel
@@ -35,10 +36,12 @@ QtObject:
     case collectibleRole:
     of CollectiblesRoles.Name: result = newQVariant(collectible.name)
     of CollectiblesRoles.Image: result = newQVariant(collectible.image)
+    of CollectiblesRoles.CollectibleId: result = newQVariant(collectible.id)
 
   method roleNames(self: CollectiblesList): Table[int, string] =
     { CollectiblesRoles.Name.int:"name",
-    CollectiblesRoles.Image.int:"image" }.toTable
+    CollectiblesRoles.Image.int:"image",
+    CollectiblesRoles.CollectibleId.int:"collectibleId" }.toTable
 
   proc addCollectibleToList*(self: CollectiblesList, colelctible: Collectible) =
     self.beginInsertRows(newQModelIndex(), self.collectibles.len, self.collectibles.len)
