95beeae131fdc3842f49728bd43712eba7d70063
Iuri Matias <iuri.matias@gmail.com>
2020-06-18 13:28:41 -0400

    fix profile name key assignment

--- src/status/profile/profile.nim
+++ src/status/profile/profile.nim
@@ -9,33 +9,34 @@ type Profile* = ref object
   systemTags*: seq[string]
 
 proc toProfileModel*(account: Account): Profile =
-    result = Profile(
-      id: "",
-      username: account.name,
-      identicon: account.photoPath,
-      alias: account.name,
-      ensName: "",
-      ensVerified: false,
-      ensVerifiedAt: 0,
-      ensVerificationRetries: 0,
-      systemTags: @[]
-    )
+  result = Profile(
+    id: "",
+    username: account.name,
+    identicon: account.photoPath,
+    alias: account.name,
+    ensName: "",
+    ensVerified: false,
+    ensVerifiedAt: 0,
+    ensVerificationRetries: 0,
+    systemTags: @[]
+  )
 
 proc toProfileModel*(profile: JsonNode): Profile =
-    var systemTags: seq[string] = @[]
-    if profile["systemTags"].kind != JNull:
-      systemTags = profile["systemTags"].to(seq[string])
+  var systemTags: seq[string] = @[]
+  if profile["systemTags"].kind != JNull:
+    systemTags = profile["systemTags"].to(seq[string])
 
-    result = Profile(
-      id: profile["id"].str,
-      username: profile["alias"].str,
-      identicon: profile["identicon"].str,
-      address: profile["id"].str,
-      alias: profile["alias"].str,
-      # ensName: profile["name"].str, // doesn't seems to exist
-      ensName: "",
-      ensVerified: profile["ensVerified"].getBool,
-      ensVerifiedAt: profile["ensVerifiedAt"].getInt,
-      ensVerificationRetries: profile["ensVerificationRetries"].getInt,
-      systemTags: systemTags
-    )
+  result = Profile(
+    id: profile["id"].str,
+    username: profile["alias"].str,
+    identicon: profile["identicon"].str,
+    address: profile["id"].str,
+    alias: profile["alias"].str,
+    ensName: "",
+    ensVerified: profile["ensVerified"].getBool,
+    ensVerifiedAt: profile["ensVerifiedAt"].getInt,
+    ensVerificationRetries: profile["ensVerificationRetries"].getInt,
+    systemTags: systemTags
+  )
+  if profile.hasKey("name"):
+    result.ensName = profile["name"].str
