8decf4cb93095bbe7bb21bfd7228be375fd29e1e
Richard Ramos <info@richardramos.me>
2020-07-15 17:05:30 -0400

    Enable OpenGL

--- vendor/nimqml/src/nimqml/private/qcoreapplication.nim
+++ vendor/nimqml/src/nimqml/private/qcoreapplication.nim
@@ -1,5 +1,8 @@
 proc enableHDPI*() =
   dos_qapplication_enable_hdpi()
+
+proc initializeOpenGL*() =
+  dos_qapplication_initialize_opengl()
   
 proc applicationDirPath*(app: QCoreApplication): string =
   let str = dos_qcoreapplication_application_dir_path()
