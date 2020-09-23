52d394ded462589a4e7aec1b97cfe93a64baba92
Iuri Matias <iuri.matias@gmail.com>
2020-09-23 09:51:49 -0400

    update join chat suggested channels to include the full list of categories and channels

---
+++ ui/app/AppLayouts/Chat/data/channelList.js
@@ -0,0 +1,154 @@
+var categories = [
+    {
+        name: "Featured chats",
+        channels: [
+            "status",
+            "support",
+            "crypto",
+            "chitchat",
+            "defi",
+            "markets",
+            "dap-ps",
+            "devcon",
+            "eth2"
+        ]
+    },
+    {
+        name: "General",
+        channels: [
+            "chitchat",
+            "chitchat",
+            "hello",
+            "worldnews",
+            "status",
+            "support"
+        ]
+    },
+    {
+        name: "Entertainment",
+        channels: [
+            "music",
+            "movies",
+            "podcasts",
+            "books",
+            "gaming",
+            "adult",
+            "tv-shows"
+        ]
+    },
+    {
+        name: "Interests",
+        channels: [
+            "sports",
+            "travel",
+            "design",
+            "food",
+            "automotive"
+        ]
+    },
+    {
+        name: "Society",
+        channels: [
+            "climatechange",
+            "blacklivesmatter",
+            "politics",
+            "hongkong",
+            "privacy"
+        ]
+    },
+    {
+        name: "Crypto",
+        channels: [
+            "crypto",
+            "markets",
+            "crypto-education",
+            "ethereum",
+            "bitcoin",
+            "chainlink",
+            "avalanche",
+            "eth2",
+            "eips",
+            "dap-ps",
+            "cryptolife",
+            "governance",
+            "staking",
+            "defi",
+            "cryptopayments",
+            "tokenomics",
+            "web3",
+            "web3design",
+            "devcon",
+            "exchange",
+            "validators"
+        ]
+    },
+    {
+        name: "Technologies",
+        channels: [
+            "tech",
+            "ai",
+            "vr-ar",
+            "networks"
+        ]
+    },
+    {
+        name: "Status",
+        channels: [
+            "status",
+            "support",
+            "statusphere",
+            "status-townhall-questions",
+            "status-core-ui",
+            "status-keycard",
+            "nimbus-general",
+            "status-assemble",
+            "status-marketing",
+            "status-protocol",
+            "status-desktop",
+            "status-watercooler",
+            "status-security",
+            "waku",
+            "status-docs",
+            "status-general",
+            "status-design",
+            "status-core-ui"
+        ]
+    },
+    {
+        name: "Development",
+        channels: [
+            "ethereum-clients",
+            "storage",
+            "indexing",
+            "sidechains",
+            "layer2",
+            "devops",
+            "smart-contracts",
+            "embark-community",
+            "subspace",
+            "open-source",
+            "security"
+        ]
+    },
+    {
+        name: "Languages",
+        channels: [
+            "status-spanish",
+            "statusbrasil",
+            "status-german",
+            "status-french",
+            "status-italiano",
+            "status-dutch",
+            "status-russian",
+            "status-chinese",
+            "status-korean",
+            "status-japanese",
+            "status-farsi",
+            "status-turkish",
+            "status-filipino",
+            "status-naija",
+            "status-indian",
+            "indonesian"
+        ]
+    }
+]
