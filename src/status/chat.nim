1b991ec6f4afcf872e1577f5c0959b36f129ff03
Jonathan Rainville <rainville.jonathan@gmail.com>
2020-12-18 15:55:33 -0500

    add import for a community

--- src/status/chat.nim
+++ src/status/chat.nim
@@ -400,3 +400,6 @@ proc inviteUserToCommunity*(self: ChatModel, communityId: string, pubKey: string
 
 proc exportCommunity*(self: ChatModel, communityId: string): string =
   result = status_chat.exportCommunity(communityId)
+
+proc importCommunity*(self: ChatModel, communityKey: string) =
+  status_chat.importCommunity(communityKey)
