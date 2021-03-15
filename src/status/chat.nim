ac122792d8bab86923469d7f08b66041f3a1f3ce
Jonathan Rainville <rainville.jonathan@gmail.com>
2021-03-15 13:19:46 -0400

    fix: fixes the ordering problem on deleting a channel
    Fixes #2035
    Also goes back to your first channel when deleting the current channel

--- src/status/chat.nim
+++ src/status/chat.nim
@@ -18,6 +18,8 @@ import ens
 logScope:
   topics = "chat-model"
 
+const backToFirstChat* = "__goBackToFirstChat"
+
 type 
   ChatUpdateArgs* = ref object of Args
     chats*: seq[Chat]
@@ -215,7 +217,7 @@ proc leave*(self: ChatModel, chatId: string) =
   self.channels.del(chatId)
   discard status_chat.clearChatHistory(chatId)
   self.events.emit("channelLeft", ChatIdArg(chatId: chatId))
-  self.events.emit("activeChannelChanged", ChatIdArg(chatId: ""))
+  self.events.emit("activeChannelChanged", ChatIdArg(chatId: backToFirstChat))
 
 proc clearHistory*(self: ChatModel, chatId: string) =
   discard status_chat.clearChatHistory(chatId)
