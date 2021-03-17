status-im/status-desktop

commit f1e83f74bcb8f3aed846bd267ddfb78ce8340be3

feat: drag and drop images
Allow up to 5 images to be dragged and dropped in to one-on-one chats and in the timeline. Can be combined with the existing upload button. The upload file dialog has been changed to allow multiple selections. Drag and dropped images adhere to the following rules, with corresponding validations messages:
- Max 5 image
- Image size must be 0.5 MB or less
- File extension must be one of [".png", ".jpg", ".jpeg", ".heif", "tif", ".tiff"]

Drag and drop and uploaded images are now also deduplicated.

src/status/libstatus/chat.nim
@@ -166,6 +166,22 @@ proc sendImageMessage*(chatId: string, image: string): string =
     }
   ])
 
+proc sendImageMessages*(chatId: string, images: var seq[string]): string =
+  let
+    preferredUsername = getSetting[string](Setting.PreferredUsername, "")
+  debugEcho ">>> [status/libstatus/chat.sendImageMessages] about to send images"
+  let imagesJson = %* images.map(image => %*
+      {
+        "chatId": chatId,
+        "contentType": ContentType.Image.int,
+        "imagePath": image,
+        "ensName": preferredUsername,
+        "text": "Update to latest version to see a nice image here!"
+      }
+    )
+  debugEcho ">>> [status/libstatus/chat.sendImageMessages] imagesJson:", $imagesJson
+  callPrivateRPC("sendChatMessages".prefix, %* [imagesJson])
+
 proc sendStickerMessage*(chatId: string, sticker: Sticker): string =
   let preferredUsername = getSetting[string](Setting.PreferredUsername, "")
   callPrivateRPC("sendChatMessage".prefix, %* [
@@ -358,10 +374,9 @@ proc pendingRequestsToJoinForCommunity*(communityId: string): seq[CommunityMembe
 
 proc myPendingRequestsToJoin*(): seq[CommunityMembershipRequest] =
   let rpcResult = callPrivateRPC("myPendingRequestsToJoin".prefix).parseJSON()
-
   var communityRequests: seq[CommunityMembershipRequest] = @[]
 
-  if rpcResult{"result"}.kind != JNull:
+  if rpcResult{"error"}.kind == JNull and rpcResult{"result"}.kind != JNull:
     for jsonCommunityReqest in rpcResult["result"]:
       communityRequests.add(jsonCommunityReqest.toCommunityMembershipRequest())
 

src/status/chat.nim
@@ -247,6 +247,10 @@ proc sendImage*(self: ChatModel, chatId: string, image: string) =
   var response = status_chat.sendImageMessage(chatId, image)
   discard self.processMessageUpdateAfterSend(response)
 
+proc sendImages*(self: ChatModel, chatId: string, images: var seq[string]) =
+  var response = status_chat.sendImageMessages(chatId, images)
+  discard self.processMessageUpdateAfterSend(response)
+
 proc sendSticker*(self: ChatModel, chatId: string, sticker: Sticker) =
   var response = status_chat.sendStickerMessage(chatId, sticker)
   self.events.emit("stickerSent", StickerArgs(sticker: sticker, save: true))

src/app/chat/view.nim
@@ -224,6 +224,24 @@ QtObject:
       error "Error sending the image", msg = e.msg
       result = fmt"Error sending the image: {e.msg}"
 
+  proc sendImages*(self: ChatsView, imagePathsArray: string): string {.slot.} =
+    result = ""
+    try:
+      var images = Json.decode(imagePathsArray, seq[string])
+      let channelId = self.activeChannel.id
+
+      for imagePath in images.mitems:
+        var image = image_utils.formatImagePath(imagePath)
+        imagePath = image_resizer(image, 2000, TMPDIR)
+
+      self.status.chat.sendImages(channelId, images)
+
+      for imagePath in images.items:
+        removeFile(imagePath)
+    except Exception as e:
+      error "Error sending images", msg = e.msg
+      result = fmt"Error sending images: {e.msg}"
+
   proc activeChannelChanged*(self: ChatsView) {.signal.}
 
   proc contextChannelChanged*(self: ChatsView) {.signal.}

src/app/utilsView/view.nim
@@ -10,6 +10,7 @@ import ../../status/libstatus/settings
 import ../../status/libstatus/wallet as status_wallet
 import ../../status/libstatus/utils as status_utils
 import ../../status/ens as status_ens
+import ../utils/image_utils
 import web3/[ethtypes, conversions]
 import stew/byteutils
 
@@ -99,3 +100,13 @@ QtObject:
 
   proc getNetworkName*(self: UtilsView): string {.slot.} =
     getCurrentNetworkDetails().name
+
+  proc getFileSize*(self: UtilsView, filename: string): string {.slot.} =
+    var f: File = nil
+    if f.open(filename.formatImagePath):
+      try:
+        result = $(f.getFileSize())
+      finally:
+        close(f)
+    else:
+      raise newException(IOError, "cannot open: " & filename)

ui/app/AppLayouts/Chat/ChatColumn.qml
@@ -317,7 +317,7 @@ StackLayout {
                 }
                 onSendMessage: {
                     if (chatInput.fileUrls.length > 0){
-                        chatsModel.sendImage(chatInput.fileUrls[0], false);
+                        chatsModel.sendImages(JSON.stringify(fileUrls));
                     }
                     let msg = chatsModel.plainText(Emoji.deparse(chatInput.textInput.text))
                     if (msg.length > 0){

ui/app/AppLayouts/Timeline/TimelineLayout.qml
@@ -71,9 +71,13 @@ ScrollView {
             anchors.top: parent.top
             anchors.topMargin: 40
             chatType: Constants.chatTypeStatusUpdate
+            imageErrorMessageLocation: StatusChatInput.ImageErrorMessageLocation.Bottom
+            z: 1
             onSendMessage: {
                 if (statusUpdateInput.fileUrls.length > 0){
-                    chatsModel.sendImage(statusUpdateInput.fileUrls[0], true);
+                    statusUpdateInput.fileUrls.forEach(url => {
+                        chatsModel.sendImage(url, true);
+                    })
                 }
                 var msg = chatsModel.plainText(Emoji.deparse(statusUpdateInput.textInput.text))
                 if (msg.length > 0){

ui/app/AppMain.qml
@@ -15,6 +15,7 @@ import Qt.labs.settings 1.0
 
 RowLayout {
     id: appMain
+    property int currentView: sLayout.currentIndex
     spacing: 0
     Layout.fillHeight: true
     Layout.fillWidth: true

ui/imports/Constants.qml
@@ -124,4 +124,10 @@ QtObject {
 
     readonly property string deepLinkPrefix: 'statusim://'
     readonly property string joinStatusLink: 'join.status.im'
+
+    readonly property int maxUploadFiles: 5
+    readonly property double maxUploadFilesizeMB: 0.5
+
+    readonly property var acceptedImageExtensions: [".png", ".jpg", ".jpeg", ".svg", ".gif"]
+    readonly property var acceptedDragNDropImageExtensions: [".png", ".jpg", ".jpeg", ".heif", "tif", ".tiff"]
 }

commit d533542a45c4230326b6c5dcc014e8db7248afb1

fix(libstatus): add safety check before accessing API response
We've introduced a regression in https://github.com/status-im/status-desktop/commit/f1e83f74b#diff-f35edd413addd14c1f81816d6b5ee2bcbdf85fa0e3295d324cb78c98e26d4327L364 where we check whether an RPC's `error` is `null`
and its `result` is not `null`.

This breaks the application with an illegal storage access,
 as in case of a successful API call, the response will have only a `result` key
 (even when it's `null`).

Since we haven't done anything with a possible `error` in the reponse even before
that change was made, this commit removes the `error` check and safe guards around
whether `result` exists.

Fixes #2062

src/status/libstatus/chat.nim
@@ -376,7 +376,7 @@ proc myPendingRequestsToJoin*(): seq[CommunityMembershipRequest] =
   let rpcResult = callPrivateRPC("myPendingRequestsToJoin".prefix).parseJSON()
   var communityRequests: seq[CommunityMembershipRequest] = @[]
 
-  if rpcResult{"error"}.kind == JNull and rpcResult{"result"}.kind != JNull:
+  if rpcResult.hasKey("result") and rpcResult{"result"}.kind != JNull:
     for jsonCommunityReqest in rpcResult["result"]:
       communityRequests.add(jsonCommunityReqest.toCommunityMembershipRequest())
 