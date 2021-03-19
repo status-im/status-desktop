status-im/status-desktop

commit d46be4f7406393b143f9fac81795894c3ed5dbc4

feat(Separator): enabled custom separator height
Sometimes, `Separator` is used inside context menus to separate groups
of actions that belong together. The separator in itself doesn't have any
padding or margins in this case because the just gets transcluded as is
in the context menu, between menu items.
There are cases in the design where a padding/margin is desired though.

This change makes that possible by wrapping the separator `Rectangle` with
another `Rectangle` which controls a custom height (if desired). The inner
rectangle is then just always vertically center.

In practice this means, existing usages of `Separator` behave exactly the same,
they don't break. In addtion one can set `Separator { height: x }` while maintaining
a 1px separator line.

ui/shared/Separator.qml
@@ -2,9 +2,16 @@ import QtQuick 2.13
 import "../imports"
 
 Rectangle {
-      id: separator
-      width: parent.width
-      height: visible ? 1 : 0
-      color: Style.current.border
-      anchors.topMargin: Style.current.padding
+    id: root
+    width: parent.width
+    height: root.visible ? 1 : 0
+    anchors.topMargin: Style.current.padding
+    color: "transparent"
+    Rectangle {
+          id: separator
+          width: parent.width
+          height: 1
+          color: Style.current.border
+          anchors.verticalCenter: parent.verticalCenter
+    }
 }

commit eb2c87f0deb30278f41fe83cf60898823487f3e1

fix(Separator): ensure custom color works as expected
This was a regression introduced in https://github.com/status-im/status-desktop/pull/2065.
The new wrapping Rectangle would get the color that is possibily passed down
to Separator. Instead it should get properly bound to the actual separator.

ui/shared/Separator.qml
@@ -1,17 +1,17 @@
 import QtQuick 2.13
 import "../imports"
 
-Rectangle {
+Item {
     id: root
+    property color color: Style.current.border
     width: parent.width
     height: root.visible ? 1 : 0
     anchors.topMargin: Style.current.padding
-    color: "transparent"
     Rectangle {
           id: separator
           width: parent.width
           height: 1
-          color: Style.current.border
+          color: root.color
           anchors.verticalCenter: parent.verticalCenter
     }
 }