29e794b8cf8665de53fa0459be3654fafdfd1988
Iuri Matias <iuri.matias@gmail.com>
2021-06-08 16:30:58 -0400

    refactor token_list usage of libstatus

--- src/status/tokens.nim
+++ src/status/tokens.nim
@@ -4,6 +4,7 @@ import
   sugar, sequtils, strutils, atomics
 
 import libstatus/tokens as status_tokens
+import libstatus/eth/contracts
 import ../eventemitter
 import signals/types
 
@@ -21,6 +22,21 @@ proc newTokensModel*(events: EventEmitter): TokensModel =
 proc getSNTAddress*(): string =
   result = status_tokens.getSNTAddress()
 
+proc getCustomTokens*(self: TokensModel, useCached: bool = true): seq[Erc20Contract] =
+  result = status_tokens.getCustomTokens(useCached)
+
 proc getSNTBalance*(account: string): string =
   result = status_tokens.getSNTBalance(account)
 
+proc tokenDecimals*(contract: Contract): int =
+  result = status_tokens.tokenDecimals(contract)
+
+proc tokenName*(contract: Contract): string =
+  result = status_tokens.tokenName(contract)
+
+proc tokensymbol*(contract: Contract): string =
+  result = status_tokens.tokensymbol(contract)
+
+export newErc20Contract
+export getErc20Contracts
+export Erc20Contract
