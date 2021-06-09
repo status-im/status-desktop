80456eaa10c2d4039577b30056374e8bde8b35b1
Iuri Matias <iuri.matias@gmail.com>
2021-06-09 10:27:21 -0400

    remove libstatus references from the wallet view

--- src/status/tokens.nim
+++ src/status/tokens.nim
@@ -25,6 +25,9 @@ proc getSNTAddress*(): string =
 proc getCustomTokens*(self: TokensModel, useCached: bool = true): seq[Erc20Contract] =
   result = status_tokens.getCustomTokens(useCached)
 
+proc removeCustomToken*(self: TokensModel, address: string) =
+  status_tokens.removeCustomToken(address)
+
 proc getSNTBalance*(account: string): string =
   result = status_tokens.getSNTBalance(account)
 
@@ -37,6 +40,13 @@ proc tokenName*(contract: Contract): string =
 proc tokensymbol*(contract: Contract): string =
   result = status_tokens.tokensymbol(contract)
 
+proc getTokenBalance*(tokenAddress: string, account: string): string = 
+  result = status_tokens.getTokenBalance(tokenAddress, account)
+
+proc getToken*(self: TokensModel, tokenAddress: string): Erc20Contract =
+    result = status_tokens.getToken(tokenAddress)
+
 export newErc20Contract
 export getErc20Contracts
 export Erc20Contract
+export getErc20ContractByAddress
