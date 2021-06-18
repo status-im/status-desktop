517ebee521042f91e4e9d0818278171c7368e2bb
Richard Ramos <info@richardramos.me>
2021-06-18 15:24:10 -0400

    use status qt mirror for linux builds

--- ci/scripts/install-qt.sh
+++ ci/scripts/install-qt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 
 if [ -z "$QT_VERSION" ]; then
     echo "Please define the QT_VERSION environment variable as desired"
@@ -10,5 +9,5 @@ set -e #quit on error
 
 # Install QT
 python3 -m pip install -U pip
-python3 -m pip install aqtinstall
-python3 -m aqt install --output "$QT_PATH" ${QT_VERSION} linux desktop -m qtwebengine
+python3 -m pip install -e 'git://github.com/miurahr/aqtinstall.git@v1.2.0b4#egg=aqtinstall'
+python3 -m aqt -c aqt.ini install --output "$QT_PATH" ${QT_VERSION} linux desktop -m qtwebengine
