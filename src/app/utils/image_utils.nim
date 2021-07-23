4065fa6f0f593e57fa4f4b8d820ece3fdbab4695
Anthony Laibe <anthony@laibe.cc>
2021-07-23 11:10:22 +0200

    fix(@desktop/chat): fix upload of image with special characters
    fixes #3034

--- src/app/utils/image_utils.nim
+++ src/app/utils/image_utils.nim
@@ -1,7 +1,8 @@
 import strutils
+import uri as uri
 
 proc formatImagePath*(imagePath: string): string =
-  result = replace(imagePath, "file://", "")
+  result = uri.decodeUrl(replace(imagePath, "file://", ""))
   if defined(windows):
     # Windows doesn't work with paths starting with a slash
     result.removePrefix('/')
