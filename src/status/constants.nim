5458b6a5e469ef1bf9443ff503475ce496f31104
Andrei Smirnov <andrei.smirnov@toptal.com>
2021-09-07 14:44:58 +0300

    fix(@desktop/updates): using timeout when checking for new version

--- src/status/constants.nim
+++ src/status/constants.nim
@@ -6,4 +6,5 @@ export KEYSTOREDIR
 export TMPDIR
 export LOGDIR
 
-const APP_UPDATES_ENS* = "desktop.status.eth" 
+const APP_UPDATES_ENS* = "desktop.status.eth"
+const CHECK_VERSION_TIMEOUT_MS* = 5000
