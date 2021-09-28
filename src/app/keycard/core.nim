8f9d300312cb60ba4a3b2c6853d6f75b26ac58d2
Michele Balistreri <michele@bitgamma.com>
2021-09-28 11:06:19 +0300

    add keycard-go to makefile

--- src/app/keycard/core.nim
+++ src/app/keycard/core.nim
@@ -1,16 +1,19 @@
 import NimQml, chronicles, std/wrapnils
 import status/status
+import status/keycard as keycardlib
 import view
 
 type KeycardController* = ref object
   view*: KeycardView
   variant*: QVariant
   status: Status
+  keycard: KeycardModel
 
 proc newController*(status: Status): KeycardController =
   result = KeycardController()
   result.status = status
-  result.view = newKeycardView(status)
+  result.keycard = keycardlib.newKeycardModel()
+  result.view = newKeycardView(status, result.keycard)
   result.variant = newQVariant(result.view)
 
 proc delete*(self: KeycardController) =
