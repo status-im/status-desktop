c5af37324c6068350756154cc095062ce2e0dabe
Iuri Matias <iuri.matias@gmail.com>
2021-10-13 17:31:04 -0400

    add profile section
    add profile module
    
    add boilerplate for profile section
    
    add profile module
    
    add profile module
    
    fix variant
    
     use accounts service
    
    get identityimage to work
    
    cleanup
    
    add other contacts services
    
    add contacts service
    
    make contact section compile with refactor
    
    fix controller and service interfaces
    
    add about section

---
+++ src/app/modules/main/profile_section/contacts/model.nim
@@ -0,0 +1,118 @@
+import NimQml, chronicles, sequtils, sugar, strutils, json
+
+import status/utils as status_utils
+import status/chat/chat
+import status/types/profile
+import status/ens as status_ens
+
+import ./models/contact_list
+
+QtObject:
+  type Model* = ref object of QObject
+    contactList*: ContactList
+    contactRequests*: ContactList
+    addedContacts*: ContactList
+    blockedContacts*: ContactList
+
+  proc setup(self: Model) =
+    self.QObject.setup
+
+  proc delete*(self: Model) =
+    self.contactList.delete
+    self.addedContacts.delete
+    self.contactRequests.delete
+    self.blockedContacts.delete
+    self.QObject.delete
+
+  proc newModel*(): Model =
+    new(result, delete)
+    result.contactList = newContactList()
+    result.contactRequests = newContactList()
+    result.addedContacts = newContactList()
+    result.blockedContacts = newContactList()
+    result.setup
+
+  proc contactListChanged*(self: Model) {.signal.}
+  proc contactRequestAdded*(self: Model, name: string, address: string) {.signal.}
+
+  proc updateContactList*(self: Model, contacts: seq[Profile]) =
+    for contact in contacts:
+      var requestAlreadyAdded = false
+      for existingContact in self.contactList.contacts:
+        if existingContact.address == contact.address and existingContact.requestReceived():
+          requestAlreadyAdded = true
+          break
+
+      self.contactList.updateContact(contact)
+      if contact.added:
+        self.addedContacts.updateContact(contact)
+
+      if contact.isBlocked():
+        self.blockedContacts.updateContact(contact)
+
+      if contact.requestReceived() and not contact.added and not contact.blocked:
+        self.contactRequests.updateContact(contact)
+
+      if not requestAlreadyAdded and contact.requestReceived():
+        # TODO add back userNameOrAlias call
+        self.contactRequestAdded(contact.username, contact.address)
+        # self.contactRequestAdded(status_ens.userNameOrAlias(contact), contact.address)
+
+    self.contactListChanged()
+
+  proc getContactList(self: Model): QVariant {.slot.} =
+    return newQVariant(self.contactList)
+
+  proc setContactList*(self: Model, contactList: seq[Profile]) =
+    self.contactList.setNewData(contactList)
+    self.addedContacts.setNewData(contactList.filter(c => c.added))
+    self.blockedContacts.setNewData(contactList.filter(c => c.blocked))
+    self.contactRequests.setNewData(contactList.filter(c => c.hasAddedUs and not c.added and not c.blocked))
+
+    self.contactListChanged()
+
+  QtProperty[QVariant] list:
+    read = getContactList
+    write = setContactList
+    notify = contactListChanged
+
+  proc getAddedContacts(self: Model): QVariant {.slot.} =
+    return newQVariant(self.addedContacts)
+
+  QtProperty[QVariant] addedContacts:
+    read = getAddedContacts
+    notify = contactListChanged
+
+  proc getBlockedContacts(self: Model): QVariant {.slot.} =
+    return newQVariant(self.blockedContacts)
+
+  QtProperty[QVariant] blockedContacts:
+    read = getBlockedContacts
+    notify = contactListChanged
+
+  proc isContactBlocked*(self: Model, pubkey: string): bool {.slot.} =
+    for contact in self.blockedContacts.contacts:
+      if contact.id == pubkey:
+        return true
+    return false
+
+  proc getContactRequests(self: Model): QVariant {.slot.} =
+    return newQVariant(self.contactRequests)
+
+  QtProperty[QVariant] contactRequests:
+    read = getContactRequests
+    notify = contactListChanged
+
+  proc isAdded*(self: Model, pubkey: string): bool {.slot.} =
+    for contact in self.addedContacts.contacts:
+      if contact.id == pubkey:
+        return true
+    return false
+
+proc contactRequestReceived*(self: Model, pubkey: string): bool {.slot.} =
+    for contact in self.contactRequests.contacts:
+      if contact.id == pubkey:
+        return true
+    return false
+
+
---
+++ src/app/modules/main/profile_section/contacts/view.nim
@@ -0,0 +1,158 @@
+import NimQml, sequtils, sugar, json, strutils
+
+# import ./item
+import ../../../../../app_service/service/contacts/dto/contacts
+import ./model
+import status/types/profile
+import models/[contact_list]
+import ./io_interface
+
+# import status/types/[identity_image, profile]
+
+import ../../../../../app_service/[main]
+import ../../../../../app_service/tasks/[qt, threadpool]
+
+type
+  LookupContactTaskArg = ref object of QObjectTaskArg
+    value: string
+
+# const lookupContactTask: Task = proc(argEncoded: string) {.gcsafe, nimcall.} =
+#   let arg = decode[LookupContactTaskArg](argEncoded)
+#   var id = arg.value
+#   if not id.startsWith("0x"):
+#     id = status_ens.pubkey(id)
+#   arg.finish(id)
+
+QtObject:
+  type
+    View* = ref object of QObject
+      delegate: io_interface.AccessInterface
+      model: Model
+      modelVariant: QVariant
+      contactToAdd*: ContactsDto
+      accountKeyUID*: string
+
+  proc delete*(self: View) =
+    self.model.delete
+    self.modelVariant.delete
+    self.QObject.delete
+
+  proc newView*(delegate: io_interface.AccessInterface): View =
+    new(result, delete)
+    result.QObject.setup
+    result.delegate = delegate
+    result.model = newModel()
+    result.modelVariant = newQVariant(result.model)
+    result.contactToAdd = ContactsDto()
+
+  proc modelChanged*(self: View) {.signal.}
+
+  proc getModel*(self: View): QVariant {.slot.} =
+    return self.modelVariant
+
+  QtProperty[QVariant] model:
+    read = getModel
+    notify = modelChanged
+
+  proc contactToAddChanged*(self: View) {.signal.}
+
+  proc getContactToAddUsername(self: View): QVariant {.slot.} =
+    var username = self.contactToAdd.alias;
+
+    if self.contactToAdd.ensVerified and self.contactToAdd.name != "":
+      username = self.contactToAdd.name
+
+    return newQVariant(username)
+
+  QtProperty[QVariant] contactToAddUsername:
+    read = getContactToAddUsername
+    notify = contactToAddChanged
+
+  proc getContactToAddPubKey(self: View): QVariant {.slot.} =
+    # TODO cofirm that id is the pubKey
+    return newQVariant(self.contactToAdd.id)
+
+  QtProperty[QVariant] contactToAddPubKey:
+    read = getContactToAddPubKey
+    notify = contactToAddChanged  
+
+  proc ensWasResolved*(self: View, resolvedPubKey: string) {.signal.}
+
+  proc ensResolved(self: View, id: string) {.slot.} =
+    echo "Resolved", id
+    self.ensWasResolved(id)
+    # if id == "":
+    #   self.contactToAddChanged()
+    #   return
+
+    # let contact = self.delegate.getContact(id)
+
+    # if contact != nil:
+    #   self.contactToAdd = contact
+    # else:
+    #   self.contactToAdd = ContactsDto(
+    #     id: id,
+    #     alias: self.delegate.generateAlias(id),
+    #     ensVerified: false
+    #   )
+    # self.contactToAddChanged()
+
+  proc lookupContact(self: View, slot: string, value: string) =
+    # TODO reimplement the ENS search with the threadpool
+    self.ensResolved(value)
+    # let arg = LookupContactTaskArg(
+    #   tptr: cast[ByteAddress](lookupContactTask),
+    #   vptr: cast[ByteAddress](self.vptr),
+    #   slot: slot,
+    #   value: value
+    # )
+    # self.appService.threadpool.start(arg)
+
+  proc lookupContact*(self: View, value: string) {.slot.} =
+    if value == "":
+      return
+
+    self.lookupContact("ensResolved", value)
+
+
+  proc addContact*(self: View, publicKey: string) {.slot.} =
+    self.delegate.addContact(publicKey)
+    # TODO add back joining of timeline
+    # self.status.chat.join(status_utils.getTimelineChatId(publicKey), ChatType.Profile, "", publicKey)
+
+  proc rejectContactRequest*(self: View, publicKey: string) {.slot.} =
+    self.delegate.rejectContactRequest(publicKey)
+
+  proc rejectContactRequests*(self: View, publicKeysJSON: string) {.slot.} =
+    let publicKeys = publicKeysJSON.parseJson
+    for pubkey in publicKeys:
+      self.rejectContactRequest(pubkey.getStr)
+
+  proc acceptContactRequests*(self: View, publicKeysJSON: string) {.slot.} =
+    let publicKeys = publicKeysJSON.parseJson
+    for pubkey in publicKeys:
+      self.addContact(pubkey.getStr)
+
+  proc changeContactNickname*(self: View, publicKey: string, nickname: string) {.slot.} =
+    var nicknameToSet = nickname
+    if (nicknameToSet == ""):
+      nicknameToSet = DELETE_CONTACT
+    self.delegate.changeContactNickname(publicKey, nicknameToSet, self.accountKeyUID)
+
+  proc unblockContact*(self: View, publicKey: string) {.slot.} =
+    self.model.contactListChanged()
+    self.delegate.unblockContact(publicKey)
+
+  proc contactBlocked*(self: View, publicKey: string) {.signal.}
+
+  proc blockContact*(self: View, publicKey: string) {.slot.} =
+    self.model.contactListChanged()
+    self.contactBlocked(publicKey)
+    self.delegate.blockContact(publicKey)
+
+  proc removeContact*(self: View, publicKey: string) {.slot.} =
+    self.delegate.removeContact(publicKey)
+    # TODO add back leaving timeline
+    # let channelId = status_utils.getTimelineChatId(publicKey)
+    # if self.status.chat.hasChannel(channelId):
+    #   self.status.chat.leave(channelId)
