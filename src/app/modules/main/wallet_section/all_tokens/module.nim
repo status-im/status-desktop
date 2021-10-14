b2ec36a4258ea7aefef9e6fd21c9538fdbbc9c29
Anthony Laibe <anthony@laibe.cc>
2021-10-14 10:42:18 +0200

    refactor(@desktop/wallet): Add getToken and controller

--- src/app/modules/main/wallet_section/all_tokens/module.nim
+++ src/app/modules/main/wallet_section/all_tokens/module.nim
@@ -1,23 +1,42 @@
-import ./io_interface, ./view
+import sequtils, sugar
+
+import ./io_interface, ./view, ./controller, ./item
+import ../../../../../app_service/service/token/service as token_service
 
 export io_interface
 
-type 
-  Module* [T: DelegateInterface] = ref object of AccessInterface
+type
+  Module* [T: io_interface.DelegateInterface] = ref object of io_interface.AccessInterface
     delegate: T
     view: View
+    controller: controller.AccessInterface
     moduleLoaded: bool
 
-proc newModule*[T](delegate: T): Module[T] =
+proc newModule*[T](delegate: T, tokenService: token_service.ServiceInterface): Module[T] =
   result = Module[T]()
   result.delegate = delegate
   result.view = newView(result)
+  result.controller = controller.newController[Module[T]](result, tokenService)
   result.moduleLoaded = false
 
 method delete*[T](self: Module[T]) =
   self.view.delete
+  self.controller.delete
 
 method load*[T](self: Module[T]) =
+  # TODO: replace chainID 1 with chain ID coming from settings
+  let tokens = self.controller.getTokens(1)
+  self.view.setItems(
+    tokens.map(t => initItem(
+      t.name,
+      t.symbol,
+      t.hasIcon,
+      t.addressAsString(),
+      t.decimals,
+      t.isCustom
+    ))
+  )
+
   self.moduleLoaded = true
 
 method isLoaded*[T](self: Module[T]): bool =
