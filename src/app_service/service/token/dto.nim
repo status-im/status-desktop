27d92f32f9d05e3c9a8b955024e800e6ccef30a8
Anthony Laibe <anthony@laibe.cc>
2021-10-14 10:42:18 +0200

    refactor(@desktop/wallet): Add getToken and controller

--- src/app_service/service/token/dto.nim
+++ src/app_service/service/token/dto.nim
@@ -4,6 +4,7 @@ include  ../../common/json_utils
 
 import
   web3/ethtypes, json_serialization
+from web3/conversions import `$`
 
 type
   Dto* = ref object of RootObj
@@ -14,12 +15,18 @@ type
     decimals*: int
     hasIcon* {.dontSerialize.}: bool
     color*: string
+    isCustom* {.dontSerialize.}: bool
 
-proc newDto*(name: string, chainId: int, address: Address, symbol: string, decimals: int, hasIcon: bool): Dto =
-  Dto(name: name, chainId: chainId, address: address, symbol: symbol, decimals: decimals, hasIcon: hasIcon)
+proc newDto*(
+  name: string, chainId: int, address: Address, symbol: string, decimals: int, hasIcon: bool, isCustom: bool = false
+): Dto =
+  Dto(
+    name: name, chainId: chainId, address: address, symbol: symbol, decimals: decimals, hasIcon: hasIcon, isCustom: isCustom
+  )
 
 proc toDto*(jsonObj: JsonNode): Dto =
   result = Dto()
+  result.isCustom = true
   discard jsonObj.getProp("name", result.name)
   discard jsonObj.getProp("chainId", result.chainId)
   discard jsonObj.getProp("address", result.address)
@@ -27,4 +34,5 @@ proc toDto*(jsonObj: JsonNode): Dto =
   discard jsonObj.getProp("decimals", result.decimals)
   discard jsonObj.getProp("color", result.color)
   
-  
+proc addressAsString*(self: Dto): string =
+  return $self.address
