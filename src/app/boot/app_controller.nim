ac789c9c9b68d4bd9c8ff5ef3d4040f037d677cc
Sale Djenic <aleksandardjenic@status.im>
2021-10-15 21:58:14 +0200

    quick fix for `production is not defined` error

--- src/app/boot/app_controller.nim
+++ src/app/boot/app_controller.nim
@@ -94,7 +94,6 @@ proc delete*(self: AppController) =
   self.communityService.delete
 
 proc startupDidLoad*(self: AppController) =
-  echo "StartupDidLoad"
   singletonInstance.engine.load(newQUrl("qrc:///main.qml"))
   # self.login.init()
   # self.onboarding.init()
