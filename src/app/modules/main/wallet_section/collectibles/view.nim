fbdae4cade52b1b2374ed2da25995f72680f8685
Anthony Laibe <anthony@laibe.cc>
2021-10-15 11:12:48 +0200

    refactor(@desktop/wallet): Init collectible service

--- src/app/modules/main/wallet_section/collectibles/view.nim
+++ src/app/modules/main/wallet_section/collectibles/view.nim
@@ -1,15 +1,32 @@
 import NimQml
 
+import ./model
+import ./io_interface
+
 QtObject:
   type
     View* = ref object of QObject
-      
-  proc setup(self: View) = 
-    self.QObject.setup
+      delegate: io_interface.AccessInterface
+      model: Model
+      modelVariant: QVariant
 
   proc delete*(self: View) =
+    self.model.delete
+    self.modelVariant.delete
     self.QObject.delete
 
-  proc newView*(): View =
+  proc newView*(delegate: io_interface.AccessInterface): View =
     new(result, delete)
-    result.setup()
+    result.QObject.setup
+    result.delegate = delegate
+    result.model = newModel()
+    result.modelVariant = newQVariant(result.model)
+
+  proc modelChanged*(self: View) {.signal.}
+
+  proc getModel(self: View): QVariant {.slot.} =
+    return self.modelVariant
+
+  QtProperty[QVariant] model:
+    read = getModel
+    notify = modelChanged
