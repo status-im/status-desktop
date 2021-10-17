11cdd608f31a82806b8563038d28fb7f443084bf
Sale Djenic <aleksandardjenic@status.im>
2021-10-17 12:59:26 +0200

    Log out the app on NodeStopped signal

--- src/app/modules/startup/private_interfaces/module_controller_delegate_interface.nim
+++ src/app/modules/startup/private_interfaces/module_controller_delegate_interface.nim
@@ -1,2 +1,5 @@
 method userLoggedIn*(self: AccessInterface) {.base.} =
   raise newException(ValueError, "No implementation available")
+
+method emitLogOut*(self: AccessInterface) {.base.} =
+  raise newException(ValueError, "No implementation available")
