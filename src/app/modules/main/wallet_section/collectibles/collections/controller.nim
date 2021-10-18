1828029ab212259fdd74c4eb4d2491d10de5484a
Anthony Laibe <anthony@laibe.cc>
2021-10-18 11:55:18 +0200

    refactor(@desktop/wallet): Add collection/item/collectibles module

---
+++ src/app/modules/main/wallet_section/collectibles/collections/controller.nim
@@ -0,0 +1,23 @@
+import ./controller_interface
+import ../../../../../../app_service/service/collectible/service as collectible_service
+
+export controller_interface
+
+type 
+  Controller*[T: controller_interface.DelegateInterface] = ref object of controller_interface.AccessInterface
+    delegate: T
+    collectibleService: collectible_service.ServiceInterface
+
+proc newController*[T](
+  delegate: T, 
+  collectibleService: collectible_service.ServiceInterface
+): Controller[T] =
+  result = Controller[T]()
+  result.delegate = delegate
+  result.collectibleService = collectibleService
+  
+method delete*[T](self: Controller[T]) =
+  discard
+
+method init*[T](self: Controller[T]) = 
+  discard
