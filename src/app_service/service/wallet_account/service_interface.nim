a876a4b9ce6650de93513067e82209b4d425c3da
Anthony Laibe <anthony@laibe.cc>
2021-10-18 13:00:56 +0200

    refactor(@desktop/wallet): Add wallet account service

---
+++ src/app_service/service/wallet_account/service_interface.nim
@@ -0,0 +1,16 @@
+import ./dto
+
+export dto
+
+type 
+  ServiceInterface* {.pure inheritable.} = ref object of RootObj
+  ## Abstract class for this service access.
+
+method delete*(self: ServiceInterface) {.base.} =
+  raise newException(ValueError, "No implementation available")
+
+method init*(self: ServiceInterface) {.base.} =
+  raise newException(ValueError, "No implementation available")
+
+method getAccounts*(self: ServiceInterface): seq[WalletAccountDto] {.base.} =
+  raise newException(ValueError, "No implementation available")
