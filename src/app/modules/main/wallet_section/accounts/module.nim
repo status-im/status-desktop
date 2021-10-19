f9be1cab6bf7e89d6d6605d7cc268bdad906a44b
Anthony Laibe <anthony@laibe.cc>
2021-10-19 17:15:03 +0200

    refactor(@desktop/wallet): load account list

--- src/app/modules/main/wallet_section/accounts/module.nim
+++ src/app/modules/main/wallet_section/accounts/module.nim
@@ -1,25 +1,50 @@
+import sequtils, sugar
 import eventemitter
 
-import ./io_interface, ./view
+import ./io_interface, ./view, ./item, ./controller
+import ../../../../../app_service/service/wallet_account/service as wallet_account_service
 
 export io_interface
 
 type 
-  Module* [T: DelegateInterface] = ref object of AccessInterface
+  Module* [T: io_interface.DelegateInterface] = ref object of io_interface.AccessInterface
     delegate: T
     view: View
+    controller: controller.AccessInterface
     moduleLoaded: bool
 
-proc newModule*[T](delegate: T, events: EventEmitter): Module[T] =
+proc newModule*[T](
+  delegate: T,
+  events: EventEmitter,
+  walletAccountService: wallet_account_service.ServiceInterface,
+): Module[T] =
   result = Module[T]()
   result.delegate = delegate
-  result.view = newView()
+  result.view = newView(result)
+  result.controller = controller.newController[Module[T]](result, walletAccountService)
   result.moduleLoaded = false
 
 method delete*[T](self: Module[T]) =
   self.view.delete
+  self.controller.delete
 
 method load*[T](self: Module[T]) =
+  let walletAccounts = self.controller.getWalletAccounts()
+
+  self.view.setItems(
+    walletAccounts.map(w => initItem(
+      w.name,
+      w.address,
+      w.path,
+      w.color,
+      w.publicKey,
+      w.walletType,
+      w.isWallet,
+      w.isChat,
+      w.getCurrencyBalance()
+    ))
+  )
+
   self.moduleLoaded = true
 
 method isLoaded*[T](self: Module[T]): bool =
