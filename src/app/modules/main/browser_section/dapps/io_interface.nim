e4cbd7a34bbd2fa5040c12d496433bedb66b8e01
Richard Ramos <info@richardramos.me>
2021-10-22 15:21:04 -0400

    refactor: dapps and permissions

--- src/app/modules/main/browser_section/dapps/io_interface.nim
+++ src/app/modules/main/browser_section/dapps/io_interface.nim
@@ -1,3 +1,5 @@
+import ../../../../../app_service/service/dapp_permissions/service_interface as dapp_permissions_service
+
 # Defines how parent module accesses this module
 include ./private_interfaces/module_base_interface
 include ./private_interfaces/module_access_interface
@@ -7,9 +9,3 @@ include ./private_interfaces/module_view_delegate_interface
 
 # Defines how this controller communicates with this module
 include ./private_interfaces/module_controller_delegate_interface
-
-# Defines how submodules of this module communicate with this module
-# will be added if needed
-include ./private_interfaces/module_provider_delegate_interface
-include ./private_interfaces/module_bookmark_delegate_interface
-include ./private_interfaces/module_dapps_delegate_interface
