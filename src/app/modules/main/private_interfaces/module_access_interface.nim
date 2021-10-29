5370c974720a38212acf59c0a4df5c6bf2750bfb
Sale Djenic <aleksandardjenic@status.im>
2021-10-29 13:10:17 +0200

    refactor(@desktop/chat-communities): asynchronous fetching messages

--- src/app/modules/main/private_interfaces/module_access_interface.nim
+++ src/app/modules/main/private_interfaces/module_access_interface.nim
@@ -1,10 +1,14 @@
 import ../../../../app_service/service/chat/service as chat_service
 import ../../../../app_service/service/community/service as community_service
+import ../../../../app_service/service/message/service as message_service
+
+import eventemitter
 
 method delete*(self: AccessInterface) {.base.} =
   raise newException(ValueError, "No implementation available")
 
-method load*(self: AccessInterface, chatService: chat_service.Service, communityService: community_service.Service,) 
+method load*(self: AccessInterface, events: EventEmitter, chatService: chat_service.Service, communityService: community_service.Service,
+  messageService: message_service.Service) 
   {.base.} =
   raise newException(ValueError, "No implementation available")
 
---
+++ src/app_service/service/message/dto/pinnedMessage.nim
@@ -0,0 +1,21 @@
+{.used.}
+
+import json
+
+import message
+
+include ../../../common/json_utils
+
+type PinnedMessageDto* = object
+  pinnedAt*: int64
+  pinnedBy*: string
+  message*: MessageDto
+
+proc toPinnedMessageDto*(jsonObj: JsonNode): PinnedMessageDto =
+  result = PinnedMessageDto()
+  discard jsonObj.getProp("pinnedAt", result.pinnedAt)
+  discard jsonObj.getProp("pinnedBy", result.pinnedBy)
+
+  var messageObj: JsonNode
+  if(jsonObj.getProp("message", messageObj)):
+    result.message = toMessageDto(messageObj)
