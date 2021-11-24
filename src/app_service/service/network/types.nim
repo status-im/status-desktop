d56802ff42d84af42b2ea4b5e01cdca90e8f960e
Jonathan Rainville <rainville.jonathan@gmail.com>
2021-11-24 11:49:18 -0500

    refactor(chat): move stickers to the new architecture
    Fixes #4060

--- src/app_service/service/network/types.nim
+++ src/app_service/service/network/types.nim
@@ -7,3 +7,40 @@ const Poa = 99
 const XDai = 100
 
 export Mainnet, Ropsten, Rinkeby, Goerli, Optimism, Poa, XDai
+
+type
+  NetworkType* {.pure.} = enum
+    Mainnet = "mainnet_rpc",
+    Testnet = "testnet_rpc",
+    Rinkeby = "rinkeby_rpc",
+    Goerli = "goerli_rpc",
+    XDai = "xdai_rpc",
+    Poa = "poa_rpc",
+    Other = "other"
+
+proc toNetworkType*(networkName: string): NetworkType =
+  case networkName:
+  of "mainnet_rpc":
+    result = NetworkType.Mainnet
+  of "testnet_rpc":
+    result = NetworkType.Testnet
+  of "rinkeby_rpc":
+    result = NetworkType.Rinkeby
+  of "goerli_rpc":
+    result = NetworkType.Goerli
+  of "xdai_rpc":
+    result = NetworkType.XDai
+  of "poa_rpc":
+    result = NetworkType.Poa
+  else:
+    result = NetworkType.Other
+
+proc toChainId*(self: NetworkType): int =
+  case self:
+    of NetworkType.Mainnet: result = Mainnet
+    of NetworkType.Testnet: result = Ropsten
+    of NetworkType.Rinkeby: result = Rinkeby
+    of NetworkType.Goerli: result = Goerli
+    of NetworkType.XDai: result = XDai
+    of NetworkType.Poa: result = 99
+    of NetworkType.Other: result = -1
