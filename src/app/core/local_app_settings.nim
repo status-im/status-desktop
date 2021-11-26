1bb2b844712effc40d7073939f228116b27140d0
Pascal Precht <pascal.precht@gmail.com>
2021-11-26 13:58:15 +0100

    feat: preserve app window size after resize
    This adds the functionality that the application window size is saved in the
    app settings, so that it restores the same size the next time the application
    is started.
    
    Closes #4120

--- src/app/core/local_app_settings.nim
+++ src/app/core/local_app_settings.nim
@@ -7,6 +7,12 @@ const LAS_KEY_LOCALE* = "global/locale"
 const DEFAULT_LOCALE = "en"
 const LAS_KEY_THEME* = "global/theme"
 const DEFAULT_THEME = 2 #system theme, from qml
+const LAS_KEY_APP_WIDTH = "global/app_width"
+const DEFAULT_APP_WIDTH = 1232
+const LAS_KEY_APP_HEIGHT = "global/app_height"
+const DEFAULT_APP_HEIGHT = 770
+const LAS_KEY_APP_SIZE_INITIALIZED = "global/app_size_initialized"
+const DEFAULT_APP_SIZE_INITIALIZED = false
 
 QtObject:
   type LocalAppSettings* = ref object of QObject
@@ -52,6 +58,41 @@ QtObject:
     write = setTheme
     notify = themeChanged 
 
+  proc appWidthChanged*(self: LocalAppSettings) {.signal.}
+  proc getAppWidth*(self: LocalAppSettings): int {.slot.} =
+    self.settings.value(LAS_KEY_APP_WIDTH, newQVariant(DEFAULT_APP_WIDTH)).intVal
+  proc setAppWidth*(self: LocalAppSettings, value: int) {.slot.} =
+    self.settings.setValue(LAS_KEY_APP_WIDTH, newQVariant(value))
+    self.appWidthChanged()
+
+  QtProperty[int] appWidth:
+    read = getAppWidth
+    write = setAppWidth
+    notify = appWidthChanged
+
+  proc appHeightChanged*(self: LocalAppSettings) {.signal.}
+  proc getAppHeight*(self: LocalAppSettings): int {.slot.} =
+    self.settings.value(LAS_KEY_APP_HEIGHT, newQVariant(DEFAULT_APP_HEIGHT)).intVal
+  proc setAppHeight*(self: LocalAppSettings, value: int) {.slot.} =
+    self.settings.setValue(LAS_KEY_APP_HEIGHT, newQVariant(value))
+    self.appHeightChanged()
+
+  QtProperty[int] appHeight:
+    read = getAppHeight
+    write = setAppHeight
+    notify = appHeightChanged
+
+  proc appSizeInitializedChanged*(self: LocalAppSettings) {.signal.}
+  proc isAppSizeInitialized*(self: LocalAppSettings): bool {.slot.} =
+    self.settings.value(LAS_KEY_APP_SIZE_INITIALIZED, newQVariant(DEFAULT_APP_SIZE_INITIALIZED)).boolVal
+  proc setAppSizeInitialized*(self: LocalAppSettings, value: bool) {.slot.} =
+    self.settings.setValue(LAS_KEY_APP_SIZE_INITIALIZED, newQVariant(value))
+    self.appSizeInitializedChanged()
+
+  QtProperty[bool] appSizeInitialized:
+    read = isAppSizeInitialized
+    write = setAppSizeInitialized
+    notify = appSizeInitializedChanged
 
   proc removeKey*(self: LocalAppSettings, key: string) =
     if(self.settings.isNil):
