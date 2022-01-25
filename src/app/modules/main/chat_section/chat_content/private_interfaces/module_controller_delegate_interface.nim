8724b3e9e731f16ef2010e40b8ae89eb5e181931
Sale Djenic <aleksandardjenic@status.im>
2022-01-25 09:55:59 +0100

    fix(@desktop/chat-messages): [base_bc] adding emoji to a message does not reflect on desktop
    Desktop app does handle message reactions properly now.
    Reactions are properly handled by the pinned messages model as well.
    
    Fixes #4525

--- src/app/modules/main/chat_section/chat_content/private_interfaces/module_controller_delegate_interface.nim
+++ src/app/modules/main/chat_section/chat_content/private_interfaces/module_controller_delegate_interface.nim
@@ -22,6 +22,10 @@ method onReactionAdded*(self: AccessInterface, messageId: string, emojiId: int, 
 method onReactionRemoved*(self: AccessInterface, messageId: string, emojiId: int, reactionId: string) {.base.} =
   raise newException(ValueError, "No implementation available")
 
+method toggleReactionFromOthers*(self: AccessInterface, messageId: string, emojiId: int, reactionId: string, 
+  reactionFrom: string) {.base.} =
+  raise newException(ValueError, "No implementation available")
+
 method onContactDetailsUpdated*(self: AccessInterface, contactId: string) {.base.} =
   raise newException(ValueError, "No implementation available")
 
