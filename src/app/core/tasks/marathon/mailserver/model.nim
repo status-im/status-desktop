05899e97f1d01c2ef010960bd302bdfa20654f00
Jonathan Rainville <rainville.jonathan@gmail.com>
2022-01-28 14:22:24 -0500

    comment out the thing that make it explode

--- src/app/core/tasks/marathon/mailserver/model.nim
+++ src/app/core/tasks/marathon/mailserver/model.nim
@@ -7,23 +7,23 @@ logScope:
 
 type
   MailserverModel* = ref object
-    mailserverController: MailserverController
+    # mailserverController: MailserverController
 
 proc newMailserverModel*(vptr: ByteAddress): MailserverModel =
   result = MailserverModel()
-  result.mailserverController = cast[MailserverController](vptr)
+  # result.mailserverController = cast[MailserverController](vptr)
 
 proc disconnectActiveMailserver(self: MailserverModel) =
   try:
     warn "Disconnecting active mailserver due to error"
-    self.mailserverController.disconnectActiveMailserver()
+    # self.mailserverController.disconnectActiveMailserver()
   except Exception as e:
     error "error: ", errDescription=e.msg
 
 proc requestMessages*(self: MailserverModel) =
   try:
     info "Requesting message history"
-    self.mailserverController.requestAllHistoricMessages()
+    # self.mailserverController.requestAllHistoricMessages()
   except Exception as e:
     error "error: ", errDescription=e.msg
     self.disconnectActiveMailserver()
@@ -31,7 +31,7 @@ proc requestMessages*(self: MailserverModel) =
 proc requestMoreMessages*(self: MailserverModel, chatId: string) =
   try:
     info "Requesting more messages for", chatId=chatId
-    self.mailserverController.syncChatFromSyncedFrom(chatId)
+    # self.mailserverController.syncChatFromSyncedFrom(chatId)
   except Exception as e:
     error "error: ", errDescription=e.msg
     self.disconnectActiveMailserver()
@@ -39,7 +39,7 @@ proc requestMoreMessages*(self: MailserverModel, chatId: string) =
 proc fillGaps*(self: MailserverModel, chatId: string, messageIds: seq[string]) =
   try:
     info "Requesting fill gaps from", chatId=chatId
-    self.mailserverController.fillGaps(chatId, messageIds)
+    # self.mailserverController.fillGaps(chatId, messageIds)
   except Exception as e:
     error "error: ", errDescription=e.msg
     self.disconnectActiveMailserver()
