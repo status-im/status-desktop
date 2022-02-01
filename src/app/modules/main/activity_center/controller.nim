2ca1b7136495c07a590e62c4629609038f392527
Anthony Laibe <anthony@laibe.cc>
2022-02-01 11:20:52 +0100

    fix(@activity_center): fix dup messages
    fixes #4631

--- src/app/modules/main/activity_center/controller.nim
+++ src/app/modules/main/activity_center/controller.nim
@@ -3,10 +3,8 @@ import ./controller_interface
 import ./io_interface
 
 import ../../../core/eventemitter
-import ../../../core/signals/types
 import ../../../../app_service/service/activity_center/service as activity_center_service
 import ../../../../app_service/service/contacts/service as contacts_service
-import ../../../../app_service/service/chat/service as chat_service
 import ../../../../app_service/service/message/service as message_service
 import ../../../../app_service/service/eth/utils as eth_utils
 
@@ -38,14 +36,9 @@ method delete*[T](self: Controller[T]) =
   discard
 
 method init*[T](self: Controller[T]) =
-  self.events.on(SIGNAL_ACTIVITY_CENTER_NOTIFICATIONS_LOADED) do(e: Args):
+  self.events.on(activity_center_service.SIGNAL_ACTIVITY_CENTER_NOTIFICATIONS_LOADED) do(e: Args):
     let args = ActivityCenterNotificationsArgs(e)
-    self.delegate.pushActivityCenterNotifications(args.activityCenterNotifications)
-
-  self.events.on(chat_service.SIGNAL_CHAT_UPDATE) do(e: Args):
-    var evArgs = ChatUpdateArgsNew(e)
-    if (evArgs.activityCenterNotifications.len > 0):
-      self.delegate.addActivityCenterNotification(evArgs.activityCenterNotifications)
+    self.delegate.addActivityCenterNotification(args.activityCenterNotifications)
 
   self.events.on(activity_center_service.SIGNAL_MARK_NOTIFICATIONS_AS_ACCEPTED) do(e: Args):
     var evArgs = MarkAsAcceptedNotificationProperties(e)
@@ -67,11 +60,6 @@ method init*[T](self: Controller[T]) =
     var evArgs = MarkAsUnreadNotificationProperties(e)
     if (evArgs.notificationIds.len > 0):
       self.delegate.markActivityCenterNotificationUnreadDone(evArgs.notificationIds)
-
-  self.events.on(SignalType.Message.event) do(e: Args):
-    var evArgs = MessageSignal(e)
-    if (evArgs.activityCenterNotifications.len > 0):
-      self.delegate.addActivityCenterNotification(evArgs.activityCenterNotifications)
 
 
 method hasMoreToShow*[T](self: Controller[T]): bool =
