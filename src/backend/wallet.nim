258273bda932a78dc94e7dbcc5640c4e52ee3545
Sale Djenic <aleksandardjenic@status.im>
2022-02-07 16:18:44 +0100

    feat(@desktop/wallet): move create accounts to `status-go`
    Fixes #4670

--- src/backend/wallet.nim
+++ src/backend/wallet.nim
@@ -10,3 +10,19 @@ logScope:
 proc getPendingTransactions*(): RpcResponse[JsonNode] {.raises: [Exception].} =
   let payload = %* []
   result = core.callPrivateRPC("wallet_getPendingTransactions", payload)
+
+proc generateAccount*(password, name, color: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [hashPassword(password), name, color]
+  return core.callPrivateRPC("accounts_generateAccount", payload)
+
+proc addAccountWithMnemonic*(mnemonic, password, name, color: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [mnemonic, hashPassword(password), name, color]
+  return core.callPrivateRPC("accounts_addAccountWithMnemonic", payload)
+
+proc addAccountWithPrivateKey*(privateKey, password, name, color: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [privateKey, hashPassword(password), name, color]
+  return core.callPrivateRPC("accounts_addAccountWithPrivateKey", payload)
+
+proc addAccountWatch*(address, name, color: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [address, name, color]
+  return core.callPrivateRPC("accounts_addAccountWatch", payload)
