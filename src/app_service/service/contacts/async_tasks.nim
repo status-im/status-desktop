4341d12efc7df225951f6a7b7b4bf840c23149f5
Sale Djenic <aleksandardjenic@status.im>
2022-02-07 15:28:19 +0100

    fix(@desktop/wallet): send transaction to address UI is broken
    Fixes #4623

--- src/app_service/service/contacts/async_tasks.nim
+++ src/app_service/service/contacts/async_tasks.nim
@@ -1,4 +1,4 @@
-import os
+import os, parseutils
 import ../ens/utils as ens_utils
 
 include ../../common/json_utils
@@ -7,6 +7,8 @@ include ../../../app/core/tasks/common
 #################################################
 # Async lookup ENS contact
 #################################################
+
+const PK_LENGTH_0X_INCLUDED = 132
 
 type
   LookupContactTaskArg = ref object of QObjectTaskArg
@@ -17,7 +19,14 @@ const lookupContactTask: Task = proc(argEncoded: string) {.gcsafe, nimcall.} =
   let arg = decode[LookupContactTaskArg](argEncoded)
   var pubkey = arg.value
   var address = ""
-  if not pubkey.startsWith("0x"):
+
+  if (pubkey.startsWith("0x")):
+    var num64: int64
+    let parsedChars = parseHex(pubkey, num64)
+    if(parsedChars != PK_LENGTH_0X_INCLUDED):
+      pubkey = ""
+      address = ""
+  else:
     # TODO refactor those calls to use the new backend and also do it in a signle call
     pubkey = ens_utils.pubkey(arg.value)
     address = ens_utils.address(arg.value)
