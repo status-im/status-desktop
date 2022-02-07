e3b349fe510903db13c60fb4bc9abf84710a9470
Sale Djenic <aleksandardjenic@status.im>
2022-02-07 12:54:29 +0100

    feat(@desktop/general): move token balance retrieval to status go
    Fixes #4677

--- src/backend/eth.nim
+++ src/backend/eth.nim
@@ -14,14 +14,6 @@ proc getEthBalance*(address: string): RpcResponse[JsonNode] {.raises: [Exception
   let payload = %* [address, "latest"]
   return core.callPrivateRPC("eth_getBalance", payload)
 
-proc getTokenBalance*(tokenAddress: string, accountAddress: string): RpcResponse[JsonNode] {.raises: [Exception].} =
-  var postfixedAccount: string = accountAddress
-  postfixedAccount.removePrefix("0x")
-  let payload = %* [{
-    "to": tokenAddress, "from": accountAddress, "data": fmt"0x70a08231000000000000000000000000{postfixedAccount}"
-  }, "latest"]
-  return core.callPrivateRPC("eth_call", payload)
-
 proc sendTransaction*(transactionData: string, password: string): RpcResponse[JsonNode] {.raises: [Exception].} =
   core.sendTransaction(transactionData, password)
 
--- src/backend/tokens.nim
+++ src/backend/tokens.nim
@@ -9,14 +9,3 @@ proc getTokens*(chainId: int): RpcResponse[JsonNode] {.raises: [Exception].} =
 
 proc getBalances*(chainId: int, accounts: seq[string], tokens: seq[string]): RpcResponse[JsonNode] {.raises: [Exception].} =
   return callPrivateRPC("wallet_getTokensBalancesForChainIDs", %* [@[chainId], accounts, tokens])
-
-proc getCustomTokens*(): RpcResponse[JsonNode] {.raises: [Exception].} =
-  return callPrivateRPC("wallet_getCustomTokens", %* [])
-
-proc addCustomToken*(address: string, name: string, symbol: string, decimals: int, color: string) {.raises: [Exception].} =
-  discard callPrivateRPC("wallet_addCustomToken", %* [
-    {"address": address, "name": name, "symbol": symbol, "decimals": decimals, "color": color}
-  ])
-
-proc removeCustomToken*(address: string) {.raises: [Exception].} =
-  discard callPrivateRPC("wallet_deleteCustomToken", %* [address])
