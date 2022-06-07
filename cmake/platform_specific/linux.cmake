feaab5426294d69bdeaac3d55680d7e4360c2fbd
Stefan <stefand@status.im>
2022-06-07 12:58:06 +0200

    chore(CPP/CI): basic CI setup for linux cmake build
    Linux dockerized build and AppDir generation for compilation errors
    
    Also added AppImage TODOs, basic setup for MacOS and Windows but they
    still need work to have at least validation running
    
    Temporary disable CPP windows/mac until limitations are
    addressed in a follow up commit
    
    Extra: add build configuration fixes for rest of the platforms
    
    closes: #5998

---
+++ cmake/platform_specific/linux.cmake
@@ -0,0 +1,3 @@
+function(configure_app_os_specific TARGET_ARG URL_ORGANIZATION_NAME DOMAIN_ARG VERSION_MAJOR VERSION_MINOR VERSION_PATCH)
+    # Noting here for now; helps simplifying main setup by removing platform if/else conditions
+endfunction()
