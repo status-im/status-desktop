8fd5f5b3bbdbb1044a0b19d2578f4ecf96aae6bb
Lukáš Tinkl <lukast@status.im>
2022-07-04 17:09:00 +0200

    fix(sandbox): enable the hover effects by default (#751)
    Similar to what we did in status desktop under
    d0a26a326e95a14a6a8a5634c0d30cd6dc648b54
    
    - since Qt 5.xy, hover is not enabled by default for QQC2, so enable it
    unconditionally as we are a desktop app anyway
    - this fixes several hover effects being broken, mostly for builtin
    components like MenuItem and some buttons (eg. the leftmost NavBar)
    where we haven't enabled those with `hoverEnabled: true` explicitely

--- ui/StatusQ/sandbox/main.cpp
+++ ui/StatusQ/sandbox/main.cpp
@@ -12,6 +12,8 @@ int main(int argc, char *argv[])
 
     SandboxApp app(argc, argv);
 
+    qputenv("QT_QUICK_CONTROLS_HOVER_ENABLED", QByteArrayLiteral("1"));
+
     app.setOrganizationName("Status");
     app.setOrganizationDomain("status.im");
     app.setApplicationName("Sandbox");
