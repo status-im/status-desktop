403a7455b345939a31a734191203ac10f76166a9
femi <femi@status.im>
2022-07-08 00:56:21 +0100

    feat(@DesktopApp): Login- 12, 18, 24 Seed Phrase
    https://github.com/status-im/status-desktop/issues/6274
    https://github.com/status-im/status-desktop/issues/6275
    https://github.com/status-im/status-desktop/issues/6276
    
    Login for 12, 18 and 24 seed Phrase

--- test/ui-test/testSuites/suite_status/tst_statusSignUp/test.py
+++ test/ui-test/testSuites/suite_status/tst_statusSignUp/test.py
@@ -1,7 +1,7 @@
 source(findFile('scripts', 'python/bdd.py'))
 
 setupHooks('../shared/scripts/bdd_hooks.py')
-collectStepDefinitions('./steps', '../shared/steps', '../shared/signUpSteps')
+collectStepDefinitions('./steps', '../shared/steps', '../shared/signUpSteps', '../shared/settingsSteps')
 
 def main():
     testSettings.throwOnFailure = True
