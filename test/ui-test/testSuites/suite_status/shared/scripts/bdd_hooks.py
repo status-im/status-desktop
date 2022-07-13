f81394fae99b862b69b0f225c7b052aae145f231
Anthony Laibe <anthony@laibe.cc>
2022-07-13 15:14:52 +0200

    fix(@test): linux test run

--- test/ui-test/testSuites/suite_status/shared/scripts/bdd_hooks.py
+++ test/ui-test/testSuites/suite_status/shared/scripts/bdd_hooks.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # This file contains hook functions to run as the .feature file is executed.
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../src/"))
+
 
 from utils.FileManager import *
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../../src/"))
 
 
 _statusDektopAppName = "nim_status_client"
