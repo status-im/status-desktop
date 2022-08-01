27c6d852bd99f346588f2cb3f4fe469205d87f65
Jakub Sokołowski <jakub@status.im>
2022-08-01 09:58:32 +0200

    nix: add shell.nix for CI builds
    This currently doesn't work becuase of lack Qml and QtQuick:
    ```
    test.qml:1 module "QtQuick" is not installed
    ```
    https://github.com/NixOS/nixpkgs/issues/51990
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ shell.nix
@@ -0,0 +1,38 @@
+{ pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/328636cafa83bcdf08b2a4f4e6b7fbcbe4ec56b3.zip";
+    sha256 = "sha256:16bzv763l44rfhmv47qlgckcv4cz7n9j3dnp43lg1k0jxxzvbcqs";
+  }) { } }:
+
+let
+  qtCustom = with pkgs.qt515;
+    env "qt-custom-${qtbase.version}" ([
+      qtbase qttools qtdeclarative
+      qtlottie qtmultimedia
+      qtquickcontrols qtquickcontrols2
+      qtsvg qtwebengine
+    ]);
+in pkgs.mkShell {
+  name = "status-desktop-build-shell";
+
+  buildInputs = with pkgs; [
+    bash curl wget git file unzip jq lsb-release
+    cmake gnumake pkg-config gnugrep qtCustom
+    pcre nss pcsclite extra-cmake-modules
+    xorg.libxcb xorg.libX11 libxkbcommon
+  ] ++ (with gst_all_1; [
+    gst-libav gstreamer
+    gst-plugins-bad  gst-plugins-base
+    gst-plugins-good gst-plugins-ugly
+  ]);
+
+  # Avoid terminal issues.
+  TERM = "xterm";
+  LANG = "en_US.UTF-8";
+  LANGUAGE = "en_US.UTF-8";
+
+  QTDIR = qtCustom;
+
+  shellHook = ''
+    export MAKEFLAGS="-j$NIX_BUILD_CORES"
+  '';
+}
