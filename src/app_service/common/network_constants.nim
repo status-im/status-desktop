6c2d2c4399d95b5c7fa8aeaa51a058c836c05ca4
Jonathan Rainville <rainville.jonathan@gmail.com>
2022-09-01 16:22:03 -0400

    change path

--- src/app_service/common/network_constants.nim
+++ src/app_service/common/network_constants.nim
@@ -29,11 +29,11 @@ const GOERLI_NETWORK_RPC_URL = $getEnv("GOERLI_NETWORK_RPC_URL")
 let GOERLI_RPC_URL_RESOLVED =
   if GOERLI_NETWORK_RPC_URL != "":
     echo "RPC URL IS DEFINED ", GOERLI_NETWORK_RPC_URL
-    "http://localhost:8577"
+    "http://127.0.0.1:8577"
     # GOERLI_NETWORK_RPC_URL
   else:
     echo "GOERLI RPC URL IS NOT DEFINED"
-    "http://localhost:8577"
+    "http://127.0.0.1:8577"
     # "https://goerli.infura.io/v3/" & INFURA_TOKEN_RESOLVED
 
 echo "USING THIS URL FOR GOERLI ", GOERLI_RPC_URL_RESOLVED
