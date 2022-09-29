b9c569599d0fb59e8ac22e63b5659bb3440353d6
Noelia <noelia@status.im>
2022-09-29 16:30:25 +0200

    test(suite_wallet): Created new suite_wallet
    - Created new `suite_wallet` where all related wallet tests will be migrated to.
    - `tst_wallet` has been moved to new `suite_wallet`.
    - `tst_transaction` has been moved to new `suite_wallet`.
    Tests inside `suite_wallet` still contain some broken tests.
    
    Closes #7627

--- test/ui-test/testSuites/suite_wallet/shared/scripts/names.py
+++ test/ui-test/testSuites/suite_wallet/shared/scripts/names.py
@@ -2,9 +2,8 @@
 
 from objectmaphelper import *
 
-#from scripts.global_names import *
 from scripts.onboarding_names import *
 from scripts.settings_names import *
 from scripts.login_names import *
 
-from sections.wallet_names import *
+from wallet_names import *
