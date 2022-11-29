f0744ed61d339f645a942adbc6429421b15cc122
Dario Gabriel Lipicar <dario@status.im>
2022-11-29 15:33:35 -0300

    fix(@desktop/wallet) make collection expansion more responsive
    Collection fetch is now non-blocking
    
    Fixes #8406

--- src/app_service/service/collectible/async_tasks.nim
+++ src/app_service/service/collectible/async_tasks.nim
@@ -5,13 +5,38 @@ type
 
 const getCollectionsTaskArg: Task = proc(argEncoded: string) {.gcsafe, nimcall.} =
   let arg = decode[GetCollectionsTaskArg](argEncoded)
-
+  let output = %* {
+    "chainId": arg.chainId,
+    "address": arg.address,
+    "collections": ""
+  }
   try:
     let response = backend.getOpenseaCollectionsByOwner(arg.chainId, arg.address)
-    arg.finish(response.result)
+    output["collections"] = response.result
   except Exception as e:
     let errDesription = e.msg
-    error "error: ", errDesription
-    arg.finish("")
+    error "error getCollectionsTaskArg: ", errDesription
+  arg.finish(output)
 
-  
+type
+  GetCollectiblesTaskArg = ref object of QObjectTaskArg
+    chainId*: int
+    address*: string
+    collectionSlug: string
+    limit: int
+
+const getCollectiblesTaskArg: Task = proc(argEncoded: string) {.gcsafe, nimcall.} =
+  let arg = decode[GetCollectiblesTaskArg](argEncoded)
+  let output = %* {
+    "chainId": arg.chainId,
+    "address": arg.address,
+    "collectionSlug": arg.collectionSlug,
+    "collectibles": ""
+  }
+  try:
+    let response = backend.getOpenseaAssetsByOwnerAndCollection(arg.chainId, arg.address, arg.collectionSlug, arg.limit)
+    output["collectibles"] = response.result
+  except Exception as e:
+    let errDesription = e.msg
+    error "error getCollectiblesTaskArg: ", errDesription
+  arg.finish(output)
