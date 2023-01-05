status-im/status-desktop

commit 17aaec2d536f496c9defa54f020afddc55cadd66

feat(@desktop/wallet): Send modal update
fixes #8609

ui/StatusQ/src/StatusQ/Controls/StatusBaseInput.qml
@@ -157,7 +157,7 @@ Item {
         \qmlproperty real StatusBaseInput::leftPadding
         This property sets the leftComponentLoader's left padding.
     */
-    property real leftPadding: leftComponentLoader.item ? 8 : 16
+    property real leftPadding: leftComponentLoader.item ? 6 : 16
     /*!
         \qmlproperty real StatusBaseInput::rightPadding
         This property sets the right padding.
@@ -308,7 +308,7 @@ Item {
                 spacing: 2
                 anchors {
                     fill: parent
-                    leftMargin: root.leftPadding ? root.leftPadding : leftComponentLoader.item ? 6 : 16
+                    leftMargin: root.leftPadding
                     rightMargin: root.rightPadding
                 }
 

ui/imports/shared/controls/AmountInputWithCursor.qml
@@ -5,6 +5,8 @@ import StatusQ.Controls 0.1
 import StatusQ.Core 0.1
 import StatusQ.Core.Theme 0.1
 
+import utils 1.0
+
 StatusInput {
     id: cursorInput
 
@@ -16,8 +18,8 @@ StatusInput {
     placeholderText: ""
     input.edit.objectName: "amountInput"
     input.edit.cursorVisible: true
-    input.edit.font.pixelSize: 32
-    input.placeholderFont.pixelSize: 32
+    input.edit.font.pixelSize: Utils.getFontSizeBasedOnLetterCount(text)
+    input.placeholderFont.pixelSize: 34
     input.leftPadding: 0
     input.rightPadding: 0
     input.topPadding: 0

src/app/modules/main/profile_section/ens_usernames/io_interface.nim
@@ -85,6 +85,10 @@ method getFiatValue*(self: AccessInterface, cryptoBalance: string, cryptoSymbol:
   {.base.} =
   raise newException(ValueError, "No implementation available")
 
+method getCryptoValue*(self: AccessInterface, fiatAmount: string, cryptoSymbol: string, fiatSymbol: string): string
+  {.base.} =
+  raise newException(ValueError, "No implementation available")
+
 method getGasEthValue*(self: AccessInterface, gweiValue: string, gasLimit: string): string {.base.} =
   raise newException(ValueError, "No implementation available")
 

src/app/modules/main/profile_section/ens_usernames/module.nim
@@ -339,6 +339,20 @@ method getFiatValue*(self: Module, cryptoBalance: string, cryptoSymbol: string, 
   let value = floatCryptoBalance * price
   return fmt"{value}"
 
+method getCryptoValue*(self: Module, fiatAmount: string, cryptoSymbol: string, fiatSymbol: string): string =
+  var fiatAmountBalance: float = 0
+  try:
+    fiatAmountBalance = parseFloat(fiatAmount)
+  except ValueError:
+    return "0.00"
+
+  if (fiatAmount == "" or cryptoSymbol == "" or fiatSymbol == ""):
+    return "0.00"
+
+  let price = self.controller.getPrice(cryptoSymbol, fiatSymbol)
+  let value = fiatAmountBalance / price
+  return fmt"{value}"
+
 method getGasEthValue*(self: Module, gweiValue: string, gasLimit: string): string {.slot.} =
   var gasLimitInt:int
 

src/app/modules/main/profile_section/ens_usernames/view.nim
@@ -126,6 +126,9 @@ QtObject:
   proc getFiatValue*(self: View, cryptoBalance: string, cryptoSymbol: string, fiatSymbol: string): string {.slot.} =
     return self.delegate.getFiatValue(cryptoBalance, cryptoSymbol, fiatSymbol)
 
+  proc getCryptoValue*(self: View, fiatAmount: string, cryptoSymbol: string, fiatSymbol: string): string {.slot.} =
+    return self.delegate.getCryptoValue(fiatAmount, cryptoSymbol, fiatSymbol)
+
   proc getGasEthValue*(self: View, gweiValue: string, gasLimit: string): string {.slot.} =
     return self.delegate.getGasEthValue(gweiValue, gasLimit)
 

src/app_service/service/transaction/async_tasks.nim
@@ -62,6 +62,39 @@ proc getFeesTotal*(paths: seq[TransactionPathDto]): Fees =
     fees.totalTime += path.estimatedTime
   return fees
 
+proc getTotalAmountToReceive*(paths: seq[TransactionPathDto]): UInt256 =
+  var totalAmountToReceive: UInt256 = stint.u256(0)
+  for path in paths:
+    totalAmountToReceive += path.amountOut
+
+  return totalAmountToReceive
+
+proc getToNetworksList*(paths: seq[TransactionPathDto]): seq[SendToNetwork] =
+  var networkMap: Table[int, SendToNetwork] = initTable[int, SendToNetwork]()
+  for path in paths:
+    if(networkMap.hasKey(path.toNetwork.chainId)):
+      networkMap[path.toNetwork.chainId].amountOut = networkMap[path.toNetwork.chainId].amountOut + path.amountOut
+    else:
+      networkMap[path.toNetwork.chainId] = SendToNetwork(chainId: path.toNetwork.chainId, chainName: path.toNetwork.chainName, iconUrl: path.toNetwork.iconURL, amountOut: path.amountOut)
+  return toSeq(networkMap.values)
+
+proc addFirstSimpleBridgeTxFlag(paths: seq[TransactionPathDto]) : seq[TransactionPathDto] =
+  let txPaths = paths
+  var firstSimplePath: bool = false
+  var firstBridgePath: bool = false
+
+  for path in txPaths:
+    if path.bridgeName == "Simple":
+      if not firstSimplePath:
+        firstSimplePath = true
+        path.isFirstSimpleTx = true
+    else:
+      if not firstBridgePath:
+        firstBridgePath = false
+        path.isFirstBridgeTx = true
+
+  return txPaths
+
 const getSuggestedRoutesTask*: Task = proc(argEncoded: string) {.gcsafe, nimcall.} =
   let arg = decode[GetSuggestedRoutesTaskArg](argEncoded)
 
@@ -86,16 +119,17 @@ const getSuggestedRoutesTask*: Task = proc(argEncoded: string) {.gcsafe, nimcall
     bestPaths.sort(sortAsc[TransactionPathDto])
     let output = %*{
       "suggestedRoutes": SuggestedRoutesDto(
-        best: bestPaths,
-        candidates: response["Candidates"].getElems().map(x => x.toTransactionPathDto()),
-        gasTimeEstimate: getFeesTotal(bestPaths)),
+        best: addFirstSimpleBridgeTxFlag(bestPaths),
+        gasTimeEstimate: getFeesTotal(bestPaths),
+        amountToReceive: getTotalAmountToReceive(bestPaths),
+        toNetworks: getToNetworksList(bestPaths)),
       "error": ""
     }
     arg.finish(output)
 
   except Exception as e:
     let output = %* {
-     "suggestedRoutes": SuggestedRoutesDto(best: @[], candidates: @[], gasTimeEstimate:  Fees()),
+     "suggestedRoutes": SuggestedRoutesDto(best: @[], gasTimeEstimate:  Fees(), amountToReceive: stint.u256(0), toNetworks: @[]),
       "error": fmt"Error getting suggested routes: {e.msg}"
     }
     arg.finish(output)

src/app_service/service/transaction/dto.nim
@@ -164,7 +164,8 @@ type
     cost*: float
     estimatedTime*: int
     amountInLocked*: bool
-
+    isFirstSimpleTx*: bool
+    isFirstBridgeTx*: bool
 
 proc `$`*(self: TransactionPathDto): string =
   return fmt"""TransactionPath(
@@ -179,7 +180,9 @@ proc `$`*(self: TransactionPathDto): string =
     bonderFees:{self.bonderFees},
     cost:{self.cost},
     estimatedTime:{self.estimatedTime},
-    amountInLocked:{self.amountInLocked}
+    amountInLocked:{self.amountInLocked},
+    isFirstSimpleTx:{self.isFirstSimpleTx},
+    isFirstBridgeTx:{self.isFirstBridgeTx}
   )"""
 
 proc toTransactionPathDto*(jsonObj: JsonNode): TransactionPathDto =
@@ -197,6 +200,8 @@ proc toTransactionPathDto*(jsonObj: JsonNode): TransactionPathDto =
   result.estimatedTime = jsonObj{"EstimatedTime"}.getInt
   discard jsonObj.getProp("GasAmount", result.gasAmount)
   discard jsonObj.getProp("AmountInLocked", result.amountInLocked)
+  result.isFirstSimpleTx = false
+  result.isFirstBridgeTx = false
 
 proc convertToTransactionPathDto*(jsonObj: JsonNode): TransactionPathDto =
   result = TransactionPathDto()
@@ -220,7 +225,15 @@ type
     totalTime*: int
 
 type
+  SendToNetwork* = ref object
+    chainId*: int
+    chainName*: string
+    iconUrl*: string
+    amountOut*: UInt256
+
+type
   SuggestedRoutesDto* = ref object
     best*: seq[TransactionPathDto]
-    candidates*: seq[TransactionPathDto]
     gasTimeEstimate*: Fees
+    amountToReceive*: UInt256
+    toNetworks*: seq[SendToNetwork]

ui/app/AppLayouts/Profile/stores/EnsUsernamesStore.qml
@@ -122,6 +122,12 @@ QtObject {
         return ensUsernamesModule.getFiatValue(balance, cryptoSymbol, fiatSymbol)
     }
 
+    function getCryptoValue(balance, cryptoSymbol, fiatSymbol) {
+        if(!root.ensUsernamesModule)
+            return ""
+        return ensUsernamesModule.getCryptoValue(balance, cryptoSymbol, fiatSymbol)
+    }
+
     function getGasEthValue(gweiValue, gasLimit) {
         if(!root.ensUsernamesModule)
             return ""

commit c6202904699143a52a86f94dffac8a5dd965f8d5

fix(StatusBaseInput): No padding for input fields
- make the left/right padding and internal spacing consistent with the
Figma design

- the padding itself got broken by a behavior-incompatible change introduced in
https://github.com/status-im/status-desktop/commit/17aaec2d536f496c9defa54f020afddc55cadd66#diff-451194c72ab50ea2586b6f1d6521b81d9a93206069788117326788be28b638fd;
this change is however correct but we can no longer rely on the
implementation detail that used to "reset" the left/right padding to its
default value when we set  `leftPadding: 0`

Fixes #8910

ui/StatusQ/src/StatusQ/Controls/StatusBaseInput.qml
@@ -157,12 +157,12 @@ Item {
         \qmlproperty real StatusBaseInput::leftPadding
         This property sets the leftComponentLoader's left padding.
     */
-    property real leftPadding: leftComponentLoader.item ? 6 : 16
+    property real leftPadding: leftComponentLoader.status === Loader.Ready && leftComponentLoader.item ? 6 : 16
     /*!
         \qmlproperty real StatusBaseInput::rightPadding
         This property sets the right padding.
     */
-    property real rightPadding: 16
+    property real rightPadding: rightComponentLoader.status === Loader.Ready && rightComponentLoader.item ? 6 : 16
     /*!
         \qmlproperty real StatusBaseInput::topPadding
         This property sets the top padding.
@@ -305,7 +305,7 @@ Item {
                 root.editClicked()
             }
             RowLayout {
-                spacing: 2
+                spacing: 8
                 anchors {
                     fill: parent
                     leftMargin: root.leftPadding
@@ -329,7 +329,6 @@ Item {
                     id: flick
                     Layout.fillWidth: true
                     Layout.fillHeight: true
-                    Layout.leftMargin: !!leftComponentLoader.item ? 8 : 0
                     Layout.topMargin: root.topPadding
                     Layout.bottomMargin: root.bottomPadding
                     contentWidth: edit.paintedWidth
@@ -412,13 +411,13 @@ Item {
                             font.pixelSize: 15
                             wrapMode: root.multiline ? Text.WordWrap : Text.NoWrap
                             elide: StatusBaseText.ElideRight
-                            font.family: Theme.palette.baseFont.name
                             color: root.enabled ? Theme.palette.baseColor1 : Theme.palette.directColor6
                         }
                     }
                 } // Flickable
 
                 Loader {
+                    id: rightComponentLoader
                     sourceComponent: {
                         if (root.rightComponent) return root.rightComponent
                         if (root.clearable) return clearButton

ui/app/AppLayouts/Chat/controls/community/CommunityDescriptionInput.qml
@@ -10,8 +10,6 @@ import StatusQ.Controls.Validators 0.1
 StatusInput {
     id: root
 
-    leftPadding: 0
-    rightPadding: 0
     label: qsTr("Give it a short description")
     charLimit: 140
 

ui/app/AppLayouts/Chat/controls/community/CommunityIntroMessageInput.qml
@@ -10,8 +10,6 @@ import StatusQ.Controls.Validators 0.1
 StatusInput {
     id: root
 
-    leftPadding: 0
-    rightPadding: 0
     label: qsTr("Community introduction and rules")
     charLimit: 1400
 

ui/app/AppLayouts/Chat/controls/community/CommunityNameInput.qml
@@ -10,8 +10,6 @@ import StatusQ.Controls.Validators 0.1
 StatusInput {
     id: root
 
-    leftPadding: 0
-    rightPadding: 0
     label: qsTr("Name your community")
     charLimit: 30
     placeholderText: qsTr("A catchy name")

ui/app/AppLayouts/Chat/controls/community/CommunityOutroMessageInput.qml
@@ -10,8 +10,6 @@ import StatusQ.Controls.Validators 0.1
 StatusInput {
     id: root
 
-    leftPadding: 0
-    rightPadding: 0
     label: qsTr("Leaving community message")
     charLimit: 80
 

ui/app/AppLayouts/Chat/popups/community/CreateChannelPopup.qml
@@ -115,7 +115,7 @@ StatusDialog {
                     }
                 }
                 input.asset.color: colorDialog.color.toString()
-                rightPadding: 6
+                leftPadding: 16
                 input.rightComponent: StatusRoundButton {
                     objectName: "StatusChannelPopup_emojiButton"
                     implicitWidth: 32
@@ -173,7 +173,7 @@ StatusDialog {
                     // contentColor: colorDialog.colorSelected ? Theme.palette.indirectColor1 : Theme.palette.baseColor1
                     text: colorDialog.colorSelected ?
                               colorDialog.color.toString().toUpperCase() :
-                              qsTr("Pick a color")
+                              qsTr("Pick a colour")
 
                     onClicked: colorDialog.open();
                     onTextChanged: {

ui/app/AppLayouts/Chat/popups/community/TransferOwnershipPopup.qml
@@ -39,8 +39,6 @@ StatusDialog {
 
             readonly property string elidedPkey: Utils.getElidedCommunityPK(root.privateKey)
 
-            leftPadding: 0
-            rightPadding: 0
             label: qsTr("Community private key")
 
             input.text: elidedPkey

ui/imports/shared/controls/AmountInputWithCursor.qml
@@ -14,16 +14,14 @@ StatusInput {
 
     leftPadding: 0
     rightPadding: 0
+    topPadding: 0
+    bottomPadding: 0
 
     placeholderText: ""
     input.edit.objectName: "amountInput"
     input.edit.cursorVisible: true
     input.edit.font.pixelSize: Utils.getFontSizeBasedOnLetterCount(text)
     input.placeholderFont.pixelSize: 34
-    input.leftPadding: 0
-    input.rightPadding: 0
-    input.topPadding: 0
-    input.bottomPadding: 0
     input.edit.padding: 0
     input.background.color: "transparent"
     input.background.border.width: 0

ui/imports/shared/controls/SearchBox.qml
@@ -6,6 +6,4 @@ StatusInput {
     placeholderText: qsTr("Search")
     input.asset.name: "search"
     input.clearable: true
-    leftPadding: 0
-    rightPadding: 4
 }