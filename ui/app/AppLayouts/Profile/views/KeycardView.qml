status-im/status-desktop

commit 81a4d7093276d1e762cc3d3c88bbf58a4373a938

chore(Qt5.15): Fix qml warnings after qt 5.15 migration
Fixing QML Connections warnings due to deprecated onFoo handlers. Now we're using function onFoo(params).
Fixing QML compilation error due to js filename format.
Fixing cyclic dependencies between qml components.

ui/app/AppLayouts/Profile/views/KeycardView.qml
@@ -89,10 +89,10 @@ SettingsContentBase {
         Connections {
             target: root.keycardStore.keycardModule
 
-            onDisplayKeycardSharedModuleFlow: {
+            function onDisplayKeycardSharedModuleFlow() {
                 keycardPopup.active = true
             }
-            onDestroyKeycardSharedModuleFlow: {
+            function onDestroyKeycardSharedModuleFlow() {
                 keycardPopup.active = false
             }
             onSharedModuleBusy: {

ui/StatusQ/src/StatusQ/Controls/StatusSeedPhraseInput.qml
@@ -176,7 +176,7 @@ Item {
         // Consider word inserted if input looses focus while a valid word is present ("user" clicks outside)
         Connections {
             target: seedWordInput.input.edit
-            onActiveFocusChanged: {
+            function onActiveFocusChanged() {
                 if (!seedWordInput.input.edit.activeFocus && d.isInputValidWord) {
                     // There are so many side effects regarding focus and doneInsertingWord that we need to reset this flag not to be processed again.
                     d.isInputValidWord = false

ui/app/AppLayouts/Browser/BrowserLayout.qml
@@ -596,7 +596,7 @@ StatusSectionLayout {
 
     Connections {
         target: _internal.currentWebView
-        onUrlChanged: {
+        function onUrlChanged() {
             browserHeader.addressBar.text = Web3ProviderStore.obtainAddress(_internal.currentWebView.url)
             RootStore.currentTabConnected = Web3ProviderStore.hasWalletConnected(Utils.getHostname(_internal.currentWebView.url))
         }
@@ -604,14 +604,14 @@ StatusSectionLayout {
 
     Connections {
         target: BookmarksStore.bookmarksModel
-        onModelChanged: {
+        function onModelChanged() {
             browserHeader.currentFavorite = Qt.binding(function () {return BookmarksStore.getCurrentFavorite(_internal.currentWebView.url)})
         }
     }
 
     Connections {
         target: browserSection
-        onOpenUrl: {
+        function onOpenUrl(url: string) {
             root.openUrlInNewTab(url);
         }
     }

ui/app/AppLayouts/Browser/panels/DownloadBar.qml
@@ -85,7 +85,7 @@ Rectangle {
                 }
                 Connections {
                     target: downloadsMenu
-                    onCancelClicked: {
+                    function onCancelClicked() {
                         isCanceled = true
                     }
                 }

ui/app/AppLayouts/Browser/panels/DownloadView.qml
@@ -61,7 +61,7 @@ import "../controls"
             }
             Connections {
                 target: downloadsMenu
-                onCancelClicked: {
+                function onCancelClicked() {
                     isCanceled = true
                 }
             }

ui/app/AppLayouts/Browser/popups/BrowserWalletMenu.qml
@@ -102,7 +102,7 @@ Popup {
 
     Connections {
         target: WalletStore.dappBrowserAccount
-        onConnectedAccountDeleted: {
+        function onConnectedAccountDeleted() {
             popup.reload()
             // This is done because when an account is deleted and the account is updated to default one,
             // only the properties are updated and we need to listen to those events and update the selected account

ui/app/AppLayouts/Browser/views/WebProviderObj.qml
@@ -30,7 +30,7 @@ QtObject {
     property Connections conn: Connections {
         target: Web3ProviderStore.web3ProviderInst
 
-        onPostMessageResult: {
+        function onPostMessageResult(payloadMethod: string, result: string, chainId: string) {
             web3Response(result)
             const isSign = ["eth_sign", "personal_sign", "eth_signTypedData", "eth_signTypedData_v3"].indexOf(payloadMethod) > -1
             const isTx = payloadMethod === "eth_sendTransaction"

ui/app/AppLayouts/Chat/popups/community/CreateChannelPopup.qml
@@ -65,11 +65,11 @@ StatusDialog {
         enabled: root.opened && root.emojiPopupOpened
         target: emojiPopup
 
-        onEmojiSelected: function (emojiText, atCursor) {
+        function onEmojiSelected(emojiText: string, atCursor: bool) {
             nameInput.input.asset.isLetterIdenticon = false;
             nameInput.input.asset.emoji = emojiText
         }
-        onClosed: {
+        function onClosed() {
             root.emojiPopupOpened = false
         }
     }

commit 9cf99713ce67c51883662d2b6253cc3a80746508

fix(@desktop/keycard): broken keycard flows fixed
Running keycard flows was broken due to changes introduced in
commit `81a4d7093276d1e762cc3d3c88bbf58a4373a938`

ui/app/AppLayouts/Profile/views/KeycardView.qml
@@ -95,7 +95,7 @@ SettingsContentBase {
             function onDestroyKeycardSharedModuleFlow() {
                 keycardPopup.active = false
             }
-            onSharedModuleBusy: {
+            function onSharedModuleBusy() {
                 Global.openPopup(sharedModuleBusyPopupComponent)
             }
         }