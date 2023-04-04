3e550bf9a5aa816bc40e8bc91c8384abe8fb8754
Michal Iskierko <michaliskierko@gmail.com>
2023-04-04 16:11:14 +0200

    fix(@desktop/communities): Tracking airdrop transaction
    Fix #10161

---
+++ src/app_service/service/community_tokens/airdrop_details.nim
@@ -0,0 +1,29 @@
+import json
+include ../../common/json_utils
+
+type
+  AirdropDetails* = object
+    chainId*: int
+    contractAddress*: string
+    walletAddresses*: seq[string]
+    amount*: int
+
+proc toJsonNode*(self: AirdropDetails): JsonNode =
+  result = %* {
+    "chainId": self.chainId,
+    "contractAddress": self.contractAddress,
+    "walletAddresses": self.walletAddresses,
+    "amount": self.amount
+  }
+
+proc toAirdropDetails*(jsonObj: JsonNode): AirdropDetails =
+  result = AirdropDetails()
+  discard jsonObj.getProp("chainId", result.chainId)
+  discard jsonObj.getProp("contractAddress", result.contractAddress)
+  
+  var walletsObj: JsonNode
+  if(jsonObj.getProp("walletAddresses", walletsObj) and walletsObj.kind == JArray):
+    for walletAddr in walletsObj:
+      result.walletAddresses.add(walletAddr.getStr)
+
+  discard jsonObj.getProp("amount", result.amount)
