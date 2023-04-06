ae397a57c0bd752dd216615c6334f8e9096a3049
Sale Djenic <aleksandardjenic@status.im>
2023-04-06 13:52:49 +0200

    chore(@desktop/general): currency and cache dto don't need to be references of object

--- src/app_service/service/currency/dto.nim
+++ src/app_service/service/currency/dto.nim
@@ -2,7 +2,7 @@ import json
 include  ../../common/json_utils
 
 type
-  CurrencyFormatDto* = ref object
+  CurrencyFormatDto* = object
     symbol*: string
     displayDecimals*: uint
     stripTrailingZeroes*: bool
