4dd383ffb53726c324b4f12e159f092e696ff949
Jonathan Rainville <rainville.jonathan@gmail.com>
2023-05-18 11:08:01 -0400

    refactor(DnD): improve chat reorder by passing destination directly

--- src/app/modules/main/chat_section/controller.nim
+++ src/app/modules/main/chat_section/controller.nim
@@ -612,7 +612,7 @@ proc inviteUsersToCommunity*(self: Controller, pubKeys: string, inviteMessage: s
 proc reorderCommunityCategories*(self: Controller, categoryId: string, position: int) =
   self.communityService.reorderCommunityCategories(self.sectionId, categoryId, position)
 
-proc reorderCommunityChat*(self: Controller, categoryId: string, chatId: string, position: int): string =
+proc reorderCommunityChat*(self: Controller, categoryId: string, chatId: string, position: int) =
   self.communityService.reorderCommunityChat(self.sectionId, categoryId, chatId, position)
 
 proc getRenderedText*(self: Controller, parsedTextArray: seq[ParsedText], communityChats: seq[ChatDto]): string =
