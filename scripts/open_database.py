2e65dfb5dabbd40066aa2b622cb9fe62dd7417f1
Igor Sirotin <sirotin@status.im>
2023-06-20 17:11:51 +0300

    read kdfIterations from accounts database

--- scripts/open_database.py
+++ scripts/open_database.py
@@ -3,19 +3,28 @@ import sqlcipher3
 from getpass import getpass
 from common import PasswordFunctions
 
-def open_db(file_path, passwordHash: str):
+def open_db(file_path, passwordHash: str, kdfIterations: int):
     db = sqlcipher3.connect(file_path)
     pageSize = 8192 if file_path.endswith("-v4.db") else 1024
 
     print(f'> Opening database. Selected cipher_page_size: {pageSize}')
     db.execute(f'PRAGMA key = "{passwordHash}"')
     db.execute(f'PRAGMA cipher_page_size = {pageSize}') #1024 for older db, 8192 for newer
-    db.execute('PRAGMA kdf_iter = 256000')
+    db.execute(f'PRAGMA kdf_iter = {kdfIterations}')
     db.execute('PRAGMA cipher_hmac_algorithm = HMAC_SHA1')
     db.execute('PRAGMA cipher_kdf_algorithm = PBKDF2_HMAC_SHA1')
     # db.execute('PRAGMA cipher_compatibility = 3;')
 
     return db
+
+class Account:
+    def __init__(self, name, keyUid, kdfIterations):
+        self.name = name
+        self.keyUid = keyUid
+        self.kdfIterations = kdfIterations
+
+    def __str__(self):
+        return f"{self.name} - {self.keyUid}, kdfIterations: {self.kdfIterations}"
 
 class Config:
     def __init__(self):
@@ -31,19 +40,22 @@ class Config:
             
     def read_accounts(self):
         db = sqlcipher3.connect(self.base_path + '/accounts.sql')
-        self.accounts = db.execute('SELECT name, keyUid FROM accounts').fetchall()
+        accounts = db.execute('SELECT name, keyUid, kdfIterations FROM accounts').fetchall()
         db.close()
 
-        if len(self.accounts) == 0:
+        if len(accounts) == 0:
             print("no accoutns found")
             exit(0)
 
+        self.accounts = []
         print(f'> Accounts found: ')
-        for i, a in enumerate(self.accounts):
-            print(f'{i}: {a}')
+        for i, a in enumerate(accounts):
+            account = Account(a[0], a[1], a[2])
+            self.accounts.append(account)
+            print(f'{i}: {str(account)}')
 
     def find_database(self):
-        regex = re.compile(f'{self.selectedKeyUid}(\-v4)?.db$')
+        regex = re.compile(f'{self.selectedAccount.keyUid}(\-v4)?.db$')
         for _, _, files in os.walk(self.base_path):
             for file in files:
                 if regex.match(file):
@@ -55,7 +67,7 @@ class Config:
             self.account_index = int(self.args[1])
         else:
             self.account_index = int(input('> Select an account by index: '))
-        self.selectedKeyUid = self.accounts[self.account_index][1] # KeyUid is second
+        self.selectedAccount = self.accounts[self.account_index]
         self.database_path = self.base_path + self.find_database()
         print(f'selected database: {self.database_path}')
 
@@ -75,7 +87,7 @@ config.input_password()
 
 ## Select and open database
 
-db = open_db(config.database_path, config.password_hash)
+db = open_db(config.database_path, config.password_hash, config.selectedAccount.kdfIterations)
 
 ## use the `db` instance to execute queries
 
