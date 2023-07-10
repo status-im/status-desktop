5888dce800658a5c67645d496356bf92b5e9ef60
Vladimir Druzhinin <vlado@status.im>
2023-07-10 20:46:11 +0200

    test(pytest) Squishserver added
    #68

--- test/ui-pytest/tests/test_self.py
+++ test/ui-pytest/tests/test_self.py
@@ -11,3 +11,8 @@ _logger = logging.getLogger(__name__)
 def test_import_squish():
     _logger.info(str(driver.__dict__))
     driver.snooze(1)
+
+
+@pytest.mark.self
+def test_squish_server(server):
+    pass
