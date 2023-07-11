ad61356a3a987a76de4c4844732b63dcea063d66
Vladimir Druzhinin <vlado@status.im>
2023-07-11 21:23:25 +0200

    test(pytest) Main window handler added
    #70

---
+++ test/ui-pytest/gui/objects_map/main_names.py
@@ -0,0 +1,3 @@
+statusDesktop_mainWindow = {"name": "mainWindow", "type": "StatusWindow", "visible": True}
+statusDesktop_mainWindow_overlay = {"container": statusDesktop_mainWindow, "type": "Overlay", "unnamed": 1, "visible": True}
+splashScreen = {"container": statusDesktop_mainWindow, "objectName": "splashScreen", "type": "DidYouKnowSplashScreen"}
