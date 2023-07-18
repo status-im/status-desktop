d1484ed1724aea043211229592aa04f327f41b90
Vladimir Druzhinin <vlado@status.im>
2023-07-18 21:33:45 +0200

    test(pytest) The Methods to search color on image added
    #80

--- test/ui-pytest/constants/__init__.py
+++ test/ui-pytest/constants/__init__.py
@@ -1,2 +1,3 @@
+from . colors import *
 from . user import *
 from . tesseract import *
---
+++ test/ui-pytest/gui/components/profile_popup.py
@@ -0,0 +1,38 @@
+import constants
+import driver
+from gui.components.base_popup import BasePopup
+from gui.elements.base_object import QObject
+from gui.elements.button import Button
+from gui.elements.text_label import TextLabel
+from scripts.tools.capture.image import Image
+
+
+class ProfilePopup(BasePopup):
+
+    def __init__(self):
+        super(ProfilePopup, self).__init__()
+        self._profile_image = QObject('ProfileHeader_userImage')
+        self._user_name_label = TextLabel('ProfilePopup_displayName')
+        self._edit_profile_button = Button('ProfilePopup_editButton')
+        self._chat_key_text_label = TextLabel('https_status_app_StatusBaseText')
+        self._emoji_hash = QObject('profileDialog_userEmojiHash_EmojiHash')
+
+    @property
+    def user_name(self) -> str:
+        return self._user_name_label.text
+
+    @property
+    def chat_key(self) -> str:
+        return self._chat_key_text_label.text.split('https://status.app/u/')[1].strip()
+
+    @property
+    def emoji_hash(self) -> Image:
+        return self._emoji_hash.image
+
+    def is_user_image_contains(self, text: str):
+        # To remove all artifacts, the image cropped.
+        self._profile_image.image.crop(
+            driver.UiTypes.ScreenRectangle(
+                15, 15, self._profile_image.image.width-30, self._profile_image.image.height-30
+            ))
+        return self._profile_image.image.has_text(text, constants.tesseract.text_on_profile_image)
