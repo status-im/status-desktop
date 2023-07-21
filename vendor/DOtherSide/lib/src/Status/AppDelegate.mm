0643644aa41e6006d92bba1b1b62d2c216807c30
Igor Sirotin <sirotin@status.im>
2023-07-21 16:28:28 +0300

    wip

--- vendor/DOtherSide/lib/src/Status/AppDelegate.mm
+++ vendor/DOtherSide/lib/src/Status/AppDelegate.mm
@@ -2,8 +2,11 @@
 
 #include <QString>
 #include <QUrl>
+#include <QDebug>
+#include <iostream>
 
 #import <AppKit/NSApplication.h>
+#import <AppKit/NSWindow.h>
 
 
 @interface AppDelegate: NSObject <NSApplicationDelegate>
@@ -13,7 +16,9 @@ continueUserActivity:(NSUserActivity *)userActivity
 @end
 
 @implementation AppDelegate
-- (BOOL)application:(NSApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<NSUserActivityRestoring>> *))restorationHandler {
+- (BOOL)application:(NSApplication *)application 
+        continueUserActivity:(NSUserActivity *)userActivity 
+        restorationHandler:(void (^)(NSArray<id<NSUserActivityRestoring>> *))restorationHandler {
     if (userActivity.activityType == NSUserActivityTypeBrowsingWeb) {
         NSURL *url = userActivity.webpageURL;
         if (!url) {
@@ -26,6 +31,43 @@ continueUserActivity:(NSUserActivity *)userActivity
     return FALSE;
 }
 
+- (BOOL)applicationShouldHandleReopen:(NSApplication *)theApplication 
+        hasVisibleWindows:(BOOL)visibleWindows
+{   
+    qWarning() << "<<< applicationShouldHandleReopen" << visibleWindows;
+    auto window = theApplication.windows[0];
+
+    if (visibleWindows) {
+        [window orderFront:self];
+    }
+    else {
+        [window makeKeyAndOrderFront:self];
+    }
+
+    return YES;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+    NSApplication* app = notification.object;
+    app.windows[0].releasedWhenClosed = YES;
+    qWarning() << "<<< applicationDidFinishLaunching" << app.windows.count;
+
+    // for (id const window in app.windows)
+    // {
+    //     // window.ReleasedWhenClosed = TRUE;
+    //     [window releasedWhenClosed:TRUE]
+    // }
+
+    // [self reclaimResourcesForPort:notification.object];
+}
+
+- (BOOL)windowShouldClose:(NSWindow *)sender
+{
+    qWarning() << "<<< windowShouldClose";
+    return YES;
+}
+
 @end
 
 namespace app_delegate {
@@ -33,6 +75,8 @@ namespace app_delegate {
 void install()
 {
     NSApplication* applicationShared = [NSApplication sharedApplication];
+    // applicationShared.windows[0].releasedWhenClosed = TRUE;
+    qWarning() << "<<< created applicationShared";
     [applicationShared setDelegate:([[[AppDelegate alloc] init] autorelease])];
 }
 
