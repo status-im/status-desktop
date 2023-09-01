20de907982e614fd4d1c804a0dcefe9d96816cdf
Valentina1133 <141633821+Valentina1133@users.noreply.github.com>
2023-09-01 18:18:04 +0700

    test(Wallet) Test Manage a saved address automated (#12034)

---
+++ test/ui-pytest/gui/components/signing_phrase_popup.py
@@ -0,0 +1,16 @@
+import allure
+
+from gui.components.base_popup import BasePopup
+from gui.elements.qt.button import Button
+
+
+class SigningPhrasePopup(BasePopup):
+
+    def __init__(self):
+        super(SigningPhrasePopup, self).__init__()
+        self._ok_got_it_button = Button('signPhrase_Ok_Button')
+
+    @allure.step('Confirm signing phrase in popup')
+    def confirm_phrase(self):
+        self._ok_got_it_button.click()
+        SigningPhrasePopup().wait_until_hidden()
--- test/ui-pytest/gui/objects_map/__init__.py
+++ test/ui-pytest/gui/objects_map/__init__.py
@@ -4,3 +4,4 @@ from .main_names import *
 from .onboarding_names import *
 from .os_names import *
 from .settings_names import *
+from .wallet_names import *
