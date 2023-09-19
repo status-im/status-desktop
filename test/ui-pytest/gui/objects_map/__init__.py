a22a22da4f925f5d8ae5f9f408b4fe084b5ef0ca
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2023-09-19 16:10:19 +0300

    chore(@tests): remove ui_pytest
    QA team has migrated the pytest framework to https://github.com/status-im/desktop-qa-automation.
    This change is needed to clean up unnecessary code and to separate the code of a product and code for tests

--- test/ui-pytest/gui/objects_map/__init__.py
+++
@@ -1,7 +0,0 @@
-from .community_names import *
-from .component_names import *
-from .main_names import *
-from .onboarding_names import *
-from .os_names import *
-from .settings_names import *
-from .wallet_names import *
