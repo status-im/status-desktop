52cf93d3b1c3e5c4fc3c0da8d34229741e577585
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2023-09-19 16:10:19 +0300

    chore(@tests): remove ui_pytest
    QA team has migrated the pytest framework to https://github.com/status-im/desktop-qa-automation.
    This change is needed to clean up unnecessary code and to separate the code of a product and code for tests

--- test/ui-pytest/constants/wallet.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class DerivationPath(Enum):
-    CUSTOM = 'Custom'
-    ETHEREUM = 'Ethereum'
-    ETHEREUM_ROPSTEN = 'Ethereum Testnet (Ropsten)'
-    ETHEREUM_LEDGER = 'Ethereum (Ledger)'
-    ETHEREUM_LEDGER_LIVE = 'Ethereum (Ledger Live/KeepKey)'
