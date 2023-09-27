f48493a5e4ea16f0ecc7fa055bc1f3b6e42d0eef
Michał Cieślak <michalcieslak@status.im>
2023-09-27 12:56:10 +0200

    feat(Storybook): Track pages dir in PagesModel

--- storybook/pagesmodel.h
+++ storybook/pagesmodel.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include <QAbstractListModel>
+#include <QDateTime>
+
+class QFileSystemWatcher;
 
 struct PagesModelItem {
+    QString path;
+    QDateTime lastModified;
     QString title;
     QString category;
 };
@@ -22,6 +27,14 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role) const override;
 
+    void reload();
 private:
+    QList<PagesModelItem> load();
+
+    static void readMetadata(PagesModelItem &item);
+    static void readMetadata(QList<PagesModelItem> &items);
+
+    QString m_path;
     QList<PagesModelItem> m_items;
+    QFileSystemWatcher* fsWatcher;
 };
