e56307e38f7c69e159dbd94f6203b62f097f4e75
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2023-10-17 20:20:06 +0300

    chore(@tests): skip flaky test with image comparison failure

--- test/e2e/tests/communities/test_permissions.py
+++ test/e2e/tests/communities/test_permissions.py
@@ -15,6 +15,7 @@ pytestmark = allure.suite("Communities")
                  'Manage community: Manage Permissions screen overview')
 @pytest.mark.case(703198)
 @pytest.mark.parametrize('params', [constants.community_params])
+@pytest.mark.skip(reason='https://github.com/status-im/desktop-qa-automation/issues/186')
 def test_permissions_screen_overview(main_screen: MainWindow, params):
     main_screen.create_community(params)
 
