76c04968fe9f1c8909ba06f627f155e9b8ddbff1
Jakub Sokołowski <jakub@status.im>
2023-12-01 14:57:36 +0100

    feat(ci): add LOG_LEVEL parameter for Jenkins jobs
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- test/e2e/configs/_local.ci.py
+++ test/e2e/configs/_local.ci.py
@@ -1,7 +1,7 @@
-import logging
 import os
+import logging
 
-LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.getLevelName(os.getenv('LOG_LEVEL', 'INFO'))
 UPDATE_VP_ON_FAIL = False
 DEV_BUILD = False
 AUT_PATH = os.getenv('AUT_PATH')
