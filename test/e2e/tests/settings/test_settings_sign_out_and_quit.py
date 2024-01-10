7b1e303156a30192362866ca79b9045ab6a2328b
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-01-10 17:06:47 +0300

    chore: add multiple references for tests

--- test/e2e/tests/settings/test_settings_sign_out_and_quit.py
+++ test/e2e/tests/settings/test_settings_sign_out_and_quit.py
@@ -10,7 +10,8 @@ pytestmark = marks
 
 @pytest.mark.critical
 @allure.testcase('https://ethstatus.testrail.net/index.php?/cases/view/703010', 'Settings - Sign out & Quit')
-@pytest.mark.case(703010)
+# TODO: Experimental link for testing multiple references in test rail report by nightly job. Has to be removed!
+@allure.testcase('https://ethstatus.testrail.net/index.php?/cases/view/704620', 'Wallet -> Settings -> Saved addresses: Add new saved address')
 @pytest.mark.flaky
 # reason='https://github.com/status-im/status-desktop/issues/13013'
 def test_sign_out_and_quit(aut, main_screen: MainWindow):
