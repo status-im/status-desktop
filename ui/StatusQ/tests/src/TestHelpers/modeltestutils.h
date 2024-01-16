22dcf8269f68d8224897a6471e958302c1c04b7f
Michał Cieślak <michalcieslak@status.im>
2024-01-16 10:49:52 +0100

    StatusQ: SnapshotModel testing utility

---
+++ ui/StatusQ/tests/src/TestHelpers/modeltestutils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <QAbstractListModel>
+
+
+bool isSame(const QAbstractItemModel& model1, const QAbstractItemModel& model2,
+            bool recursive = true);
+bool isSame(const QAbstractItemModel* model1, const QAbstractItemModel* model2,
+            bool recursive = true);
