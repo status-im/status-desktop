5120822172b9cf03fc1233c26edabbd55d27124a
Michał Cieślak <michalcieslak@status.im>
2024-01-16 10:31:24 +0100

    StatusQ: ModelSignalsSpy testing utility

---
+++ ui/StatusQ/tests/src/TestHelpers/modelsignalsspy.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QAbstractListModel>
+#include <QSignalSpy>
+
+class ModelSignalsSpy
+{
+public:
+    explicit ModelSignalsSpy(QAbstractItemModel* model);
+
+    const QSignalSpy columnsAboutToBeInsertedSpy;
+    const QSignalSpy columnsAboutToBeMovedSpy;
+    const QSignalSpy columnsAboutToBeRemovedSpy;
+    const QSignalSpy columnsInsertedSpy;
+    const QSignalSpy columnsMovedSpy;
+    const QSignalSpy columnsRemovedSpy;
+    const QSignalSpy dataChangedSpy;
+    const QSignalSpy headerDataChangedSpy;
+    const QSignalSpy layoutAboutToBeChangedSpy;
+    const QSignalSpy layoutChangedSpy;
+    const QSignalSpy modelAboutToBeResetSpy;
+    const QSignalSpy modelResetSpy;
+    const QSignalSpy rowsAboutToBeInsertedSpy;
+    const QSignalSpy rowsAboutToBeMovedSpy;
+    const QSignalSpy rowsAboutToBeRemovedSpy;
+    const QSignalSpy rowsInsertedSpy;
+    const QSignalSpy rowsMovedSpy;
+    const QSignalSpy rowsRemovedSpy;
+
+    int count() const;
+};
