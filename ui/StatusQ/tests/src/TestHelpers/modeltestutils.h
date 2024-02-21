2fdca69c09c0ab8959c57eacdff197cb19ea2a4e
Michał Cieślak <michalcieslak@status.im>
2024-02-21 14:35:44 +0100

    feat(StatusQ/Tests): Added isNotSame(...) method for comparing models
    It works similarly as isSame(...) but doesn't print warnings when
    mismatch is found. Thanks to that may be used conveniently whenever
    we expect differing models in a test.

--- ui/StatusQ/tests/src/TestHelpers/modeltestutils.h
+++ ui/StatusQ/tests/src/TestHelpers/modeltestutils.h
@@ -7,3 +7,8 @@ bool isSame(const QAbstractItemModel& model1, const QAbstractItemModel& model2,
             bool recursive = true);
 bool isSame(const QAbstractItemModel* model1, const QAbstractItemModel* model2,
             bool recursive = true);
+
+bool isNotSame(const QAbstractItemModel& model1, const QAbstractItemModel& model2,
+               bool recursive = true);
+bool isNotSame(const QAbstractItemModel* model1, const QAbstractItemModel* model2,
+               bool recursive = true);
