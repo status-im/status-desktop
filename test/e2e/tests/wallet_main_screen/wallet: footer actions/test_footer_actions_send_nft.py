a3fa0ebaa3501c513d022ee40aec83d7d9374e27
Valentina Novgorodtceva <valentina@status.im>
2024-05-29 19:45:06 +0700

    chore: test send divided into 2 separate files

--- test/e2e/tests/wallet_main_screen/wallet: footer actions/test_footer_actions_send_nft.py
+++ test/e2e/tests/wallet_main_screen/wallet: footer actions/test_footer_actions_send_nft.py
@@ -22,16 +22,16 @@ def keys_screen(main_window) -> KeysView:
         return welcome_screen.get_keys()
 
 
-@allure.testcase('https://ethstatus.testrail.net/index.php?/cases/view/704527',
-                 'Send: can send 0 ETH to address pasted into receiver field with Simple flow')
-@pytest.mark.case(704527)
+@allure.testcase('https://ethstatus.testrail.net/index.php?/cases/view/704602',
+                 'Send: can send ERC 721 token (collectible) to address pasted into receiver field with Simple flow')
+@pytest.mark.case(704602)
 @pytest.mark.transaction
 @pytest.mark.parametrize('user_account', [constants.user.user_with_funds])
-@pytest.mark.parametrize('receiver_account_address, amount, asset, tab', [
-    pytest.param(constants.user.user_account_one.status_address, 0, 'Ether', 'Assets')
+@pytest.mark.parametrize('tab, receiver_account_address, amount, collectible', [
+    pytest.param('Collectibles', constants.user.user_with_funds.status_address, 1, 'Panda')
 ])
 @pytest.mark.timeout(timeout=120)
-def test_wallet_send_0_eth(keys_screen, main_window, user_account, receiver_account_address, amount, asset, tab):
+def test_wallet_send_nft(keys_screen, main_window, user_account, tab, receiver_account_address, amount, collectible):
     with step('Open import seed phrase view and enter seed phrase'):
         input_view = keys_screen.open_import_seed_phrase_view().open_seed_phrase_input_view()
         input_view.input_seed_phrase(user_account.seed_phrase, True)
@@ -52,15 +52,8 @@ def test_wallet_send_0_eth(keys_screen, main_window, user_account, receiver_acco
         if not configs.system.TEST_MODE:
             BetaConsentPopup().confirm()
 
-    with step('Verify that restored account reveals correct status wallet address'):
-        wallet_settings = main_window.left_panel.open_settings().left_panel.open_wallet_settings()
-        status_acc_view = wallet_settings.open_account_in_settings('Account 1', '0')
-        address = status_acc_view.get_account_address_value()
-        assert address == user_account.status_address, \
-            f"Recovered account should have address {user_account.status_address}, but has {address}"
-        status_acc_view.click_back_button()
-
     with step('Set testnet mode'):
+        wallet_settings = main_window.left_panel.open_settings().left_panel.open_wallet_settings()
         wallet_settings.open_networks().switch_testnet_mode_toggle().turn_on_testnet_mode_in_testnet_modal()
 
     with step('Open send popup'):
@@ -71,8 +64,8 @@ def test_wallet_send_0_eth(keys_screen, main_window, user_account, receiver_acco
         wallet_account = wallet.left_panel.select_account('Account 1')
         send_popup = wallet_account.open_send_popup()
 
-    with step('Enter asset, amount and address and click send and verify Mainnet network is shown'):
-        send_popup.send(receiver_account_address, amount, asset, tab)
+    with step('Enter asset, amount and address on Collectibles tab, click send and verify Mainnet network is shown'):
+        send_popup.send(receiver_account_address, amount, collectible, tab)
         assert driver.waitFor(lambda: send_popup.is_mainnet_network_identified, configs.timeouts.UI_LOAD_TIMEOUT_SEC)
 
     with step('Enter password in authenticate popup'):
