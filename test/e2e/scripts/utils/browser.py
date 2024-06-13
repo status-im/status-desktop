12503730372d4b62e21ccbefaad2c2972b64d079
Valentina1133 <141633821+Valentina1133@users.noreply.github.com>
2024-06-13 21:18:41 +0700

    chore: more verifications added (community title and description on web page) (#15022)

--- test/e2e/scripts/utils/browser.py
+++ test/e2e/scripts/utils/browser.py
@@ -1,13 +1,25 @@
+import allure
+import requests
 import webbrowser
 
-import requests
+from bs4 import BeautifulSoup
 
 
+@allure.step('Open link in default browser')
 def open_link(url):
     webbrowser.open(url)
 
 
+@allure.step('Get response')
 def get_response(url):
     response = requests.get(
         url)
     return response
+
+
+@allure.step('Get page content')
+def get_page_content(url):
+    request = requests.get(url)
+    src = request.text
+    soup = BeautifulSoup(src, 'html.parser')
+    return soup
