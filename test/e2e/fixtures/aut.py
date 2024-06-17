dc628f300a59c7b7d6799ddf5f57e61e22dbe416
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-06-17 17:50:40 +0300

    chore: terminate process on windows

--- test/e2e/fixtures/aut.py
+++ test/e2e/fixtures/aut.py
@@ -26,7 +26,7 @@ def application_logs():
         for app_data in configs.testpath.STATUS_DATA.iterdir():
             for log in (app_data / 'logs').glob('*.log'):
                 allure.attach.file(log, name=str(log.name), attachment_type=allure.attachment_type.TEXT)
-                log.unlink()
+                log.unlink()  # FIXME: it does not work on Windows, permission error
 
 
 @pytest.fixture
