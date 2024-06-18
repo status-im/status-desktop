430f2501d467372a8926f0f795d9f97567f01621
Igor Sirotin <sirotin@status.im>
2024-06-18 15:19:10 +0100

    fix: bandwidth usage (#15231)
    * feat: added bandwidthStatsEnabled login flag
    
    * update status-go

--- src/app_service/service/accounts/dto/login_request.nim
+++ src/app_service/service/accounts/dto/login_request.nim
@@ -8,6 +8,7 @@ type
     kdfIterations*: int
     runtimeLogLevel*: string
     wakuV2Nameserver*: string
+    bandwidthStatsEnabled*: bool
     walletSecretsConfig*: WalletSecretsConfig
 
 proc toJson*(self: LoginAccountRequest): JsonNode =
@@ -16,7 +17,8 @@ proc toJson*(self: LoginAccountRequest): JsonNode =
     "keyUid": self.keyUID,
     "kdfIterations": self.kdfIterations,
     "runtimeLogLevel": self.runtimeLogLevel,
-    "wakuV2Nameserver": self.wakuV2Nameserver
+    "wakuV2Nameserver": self.wakuV2Nameserver,
+    "bandwidthStatsEnabled": self.bandwidthStatsEnabled,
   }
   for key, value in self.walletSecretsConfig.toJson().pairs():
     result[key] = value
