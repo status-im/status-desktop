9f806c5160a4ee64552a20490b27cf3071d81a73
Godfrain Jacques <gkounkou@gmail.com>
2024-06-28 13:45:21 -0700

    fix_: prevent multiple http or websocket lauches
    fixes [5436](https://github.com/status-im/status-go/issues/5436)

---
+++ src/app_service/service/accounts/dto/api_config.nim
@@ -0,0 +1,43 @@
+import json
+
+include ../../../common/net_utils
+
+type APIConfig* = object
+  apiModules*: string
+  connectorEnabled*: bool
+  httpEnabled*: bool
+  httpHost*: string
+  httpPort*: int
+  wsEnabled*: bool
+  wsHost*: string
+  wsPort*: int
+
+proc checkAndSetPort(port: Port, isEnabled: var bool) =
+  if isPortBusy(port):
+    isEnabled = false
+
+proc defaultAPIConfig*(): APIConfig =
+  result.apiModules = "connector"
+  result.connectorEnabled = true
+
+  result.httpEnabled = true
+  checkAndSetPort(Port(8545), result.httpEnabled)
+  result.httpHost = "0.0.0.0"
+  result.httpPort = 8545
+
+  result.wsEnabled = true
+  checkAndSetPort(Port(8586), result.wsEnabled)
+  result.wsHost = "0.0.0.0"
+  result.wsPort = 8586
+
+proc toJson*(self: APIConfig): JsonNode =
+  return %* {
+    "apiModules": self.apiModules,
+    "connectorEnabled": self.connectorEnabled,
+    "httpEnabled": self.httpEnabled,
+    "httpHost": self.httpHost,
+    "httpPort": self.httpPort,
+    "wsEnabled": self.wsEnabled,
+    "wsHost": self.wsHost,
+    "wsPort": self.wsPort
+  }
