6dae485a4a1b4a6794ae240eb6d4bcb9c2dc3ec7
Valentina Novgorodtceva <valentina@status.im>
2024-07-12 16:38:13 +0700

    test

--- test/e2e/configs/_local.ci.py
+++ test/e2e/configs/_local.ci.py
@@ -5,4 +5,4 @@ LOG_LEVEL = logging.getLevelName(os.getenv('LOG_LEVEL', 'INFO'))
 UPDATE_VP_ON_FAIL = False
 DEV_BUILD = False
 AUT_PATH = os.getenv('AUT_PATH')
-os.environ['STATUS_RUNTIME_USE_MOCKED_KEYCARD'] = 'False'
+# os.environ['STATUS_RUNTIME_USE_MOCKED_KEYCARD'] = 'False'
--- test/e2e/configs/_local.default.py
+++ test/e2e/configs/_local.default.py
@@ -4,5 +4,5 @@ import os
 LOG_LEVEL = logging.DEBUG
 DEV_BUILD = False
 AUT_PATH = "path to the application (.app or .AppImage)"
-os.environ['STATUS_RUNTIME_USE_MOCKED_KEYCARD'] = 'False'
+# os.environ['STATUS_RUNTIME_USE_MOCKED_KEYCARD'] = 'False'
 
