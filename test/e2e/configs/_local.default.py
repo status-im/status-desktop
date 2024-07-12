2a4e341f9d88ff5e7b4f0d3d162b89344dde1375
Valentina Novgorodtceva <valentina@status.im>
2024-07-12 16:44:29 +0700

    chore: keycard env set separately for keycard tests only

--- test/e2e/configs/_local.default.py
+++ test/e2e/configs/_local.default.py
@@ -1,6 +1,8 @@
 import logging
+import os
 
 LOG_LEVEL = logging.DEBUG
 DEV_BUILD = False
 AUT_PATH = "path to the application (.app or .AppImage)"
+os.environ['STATUS_RUNTIME_USE_MOCKED_KEYCARD'] = 'False'
 
