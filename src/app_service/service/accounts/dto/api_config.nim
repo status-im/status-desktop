df3ec7b1f8c94a803df3bf49a31dc64b0a42eda6
Igor Sirotin <sirotin@status.im>
2024-07-17 09:38:14 +0100

    chore: enable more http api modules

--- src/app_service/service/accounts/dto/api_config.nim
+++ src/app_service/service/accounts/dto/api_config.nim
@@ -17,17 +17,17 @@ proc checkAndSetPort(port: Port, isEnabled: var bool) =
     isEnabled = false
 
 proc defaultAPIConfig*(): APIConfig =
-  result.apiModules = "connector"
+  result.apiModules = "connector,waku,eth,wakuext"
   result.connectorEnabled = true
 
   result.httpEnabled = true
-  checkAndSetPort(Port(8545), result.httpEnabled)
-  result.httpHost = "0.0.0.0"
+  checkAndSetPort(Port(8548), result.httpEnabled)
+  result.httpHost = "127.0.0.1"
   result.httpPort = 8545
 
   result.wsEnabled = true
   checkAndSetPort(Port(8586), result.wsEnabled)
-  result.wsHost = "0.0.0.0"
+  result.wsHost = "127.0.0.1"
   result.wsPort = 8586
 
 proc toJson*(self: APIConfig): JsonNode =
