734c3f65bd1e1f46aaf78d801fb6e0c47cbf24be
Siddarth Kumar <siddarthkay@gmail.com>
2024-07-19 18:03:56 +0530

    use nixpkgs 24.05

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -4,10 +4,10 @@ let
   # For testing local version of nixpkgs
   #nixpkgsSrc = (import <nixpkgs> { }).lib.cleanSource "/home/jakubgs/work/nixpkgs";
 
-  # We follow the release-20.09 branch of official nixpkgs.
+  # We follow the release-24.05 branch of official nixpkgs.
   nixpkgsSrc = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/66b0db71f463164486a36dded50bedee185e45c2.tar.gz";
-    sha256 = "sha256:0wam1m12qw9rrijhvbvhm5psj2a0ksms77xzxzyr5laz94j60cb0";
+    url = "https://github.com/NixOS/nixpkgs/archive/29679bde4a9b069f76d6ff26b8d03b62e7f018f4.tar.gz";
+    sha256 = "sha256:0cdfnfr0xwcn5y677vy73parlw447krggghrf4lmcv4kmr7j2xjh";
   };
 
   # Override some packages and utilities
