86a1267f34e6375fa1dedda211c6a3c2a9eb7dc4
Anastasiya <82375995+anastasiyaig@users.noreply.github.com>
2024-08-15 14:20:09 +0300

    ci: add env vars to prevent autotests hitting 8545 (#16120)
    * chore: increase timeout for saved addresses test
    
    * ci: add env vars to prevent autotests hitting 8545 port

--- test/e2e/tests/settings/settings_wallet/test_wallet_settings_saved_addresses_add.py
+++ test/e2e/tests/settings/settings_wallet/test_wallet_settings_saved_addresses_add.py
@@ -38,7 +38,7 @@ def test_wallet_settings_add_saved_address(main_screen: MainWindow, address: str
     with step('Verify recently added saved address is present in the list'):
         assert driver.waitFor(
             lambda: name in settings_saved_addresses.get_saved_address_names_list(),
-            configs.timeouts.UI_LOAD_TIMEOUT_MSEC), f'Address: {name} not found'
+            configs.timeouts.APP_LOAD_TIMEOUT_MSEC), f'Address: {name} not found'
 
     with step('Verify toast message when adding saved address'):
         messages = main_screen.wait_for_notification()
