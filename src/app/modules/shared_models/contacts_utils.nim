f8d33656e6e195cb41ab121100f7327a45f4d107
Michał Cieślak <michalcieslak@status.im>
2024-10-01 13:20:17 +0200

    fix: add PreferredDisplayName to user_model
    - excludes logic of resolving preferred display name to shared
      contacts_utils
    - expose PreferredDisplayName role from user_model
    - improves PreferredDisplayName handling in member_model
    
    Closes: #16429

---
+++ src/app/modules/shared_models/contacts_utils.nim
@@ -0,0 +1,8 @@
+proc resolvePreferredDisplayName*(localNickName: string, ensName: string, displayName: string, alias: string): string =
+    if localNickname != "":
+      return localNickname
+    if ensName != "":
+      return ensName
+    if displayName != "":
+      return displayName
+    return alias
