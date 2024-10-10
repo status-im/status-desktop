9767a9809c652e81fef18afc2996f1b5ce9f40a5
Anton Iakimov <yakimant@gmail.com>
2024-10-10 12:19:17 +0200

    chore(nix)_: fix qtwebengine issues

--- scripts/fix_app_dir.sh
+++ scripts/fix_app_dir.sh
@@ -18,11 +18,11 @@ if [[ ! -z "${IN_NIX_SHELL}" ]]; then
 
 	patchelf --set-rpath '$ORIGIN/../../' "${APP_DIR}"/usr/lib/gstreamer1.0/gstreamer-1.0/*
 
-	patchelf --set-rpath '$ORIGIN/../lib' "${APP_DIR}/usr/libexec/QtWebEngineProcess"
+	patchelf --set-rpath '$ORIGIN/../lib' "${APP_DIR}/usr/bin/QtWebEngineProcess"
 
 	patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 \
 		"${APP_DIR}/usr/bin/nim_status_client" \
-		"${APP_DIR}/usr/libexec/QtWebEngineProcess" \
+		"${APP_DIR}/usr/bin/QtWebEngineProcess" \
 		"${APP_DIR}/usr/lib/libQt5Core.so.5" \
 		"${APP_DIR}"/usr/lib/gstreamer1.0/gstreamer-1.0/*
 fi
