c4d36993869d15f1538767aea9bb48121e43ab55
Richard Ramos <info@richardramos.me>
2024-10-10 21:21:29 -0400

    chore: libssl1.1

--- docker-linux-app-image.sh
+++ docker-linux-app-image.sh
@@ -5,7 +5,7 @@ cp -R . ~/status-desktop
 cd ~/status-desktop
 
 git clean -dfx && rm -rf vendor/* && git checkout vendor/DOtherSide && make -j4 V=1 update
-make V=1 pkg
+make V=1 pkg USE_NWAKU=true
 
 # Make AppImage build accessible to the docker host
 cd - && cp -R ~/status-desktop/pkg .
