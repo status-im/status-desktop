5b05e42486da7711a4988809c485f387b51c2b0a
Richard Ramos <info@richardramos.me>
2024-10-11 15:39:49 -0400

    chore: change negentropy c++ to 2a

--- docker-linux-app-image.sh
+++ docker-linux-app-image.sh
@@ -4,9 +4,12 @@
 cp -R . ~/status-desktop
 cd ~/status-desktop
 
+
+
 git clean -dfx && rm -rf vendor/* && git checkout vendor/DOtherSide && make -j4 V=1 update
 
-sed -i 's/c++20/c++2a/g' vendor/status-go/third_party/nwaku/vendor/negentropy/cpp/Makefile
+
+sed -i 's|\t\$(MAKE) -C \./third_party/nwaku libwaku|\tsed -i '\''s/c++20/c++2a/g'\'' ./third_party/nwaku/vendor/negentropy/cpp/Makefile; $(MAKE) -C ./third_party/nwaku libwaku|' ./vendor/status-go/Makefile
 
 make V=1 pkg USE_NWAKU=true
 
