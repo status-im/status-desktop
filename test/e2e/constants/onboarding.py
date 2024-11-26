7feed9ebb6a064522a578a14665f046112e53535
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-11-26 09:01:24 +0300

    chore(@e2e): change tests according to new designs

--- test/e2e/constants/onboarding.py
+++ test/e2e/constants/onboarding.py
@@ -23,13 +23,13 @@ class KeysExistText(Enum):
 
 password_strength_elements = namedtuple('Password_Strength_Elements',
                                         ['strength_indicator', 'strength_color', 'strength_messages'])
-very_weak_lower_elements = password_strength_elements('Very weak', '#ff2d55', ['• Lower case'])
-very_weak_upper_elements = password_strength_elements('Very weak', '#ff2d55', ['• Upper case'])
-very_weak_numbers_elements = password_strength_elements('Very weak', '#ff2d55', ['• Numbers'])
-very_weak_symbols_elements = password_strength_elements('Very weak', '#ff2d55', ['• Symbols'])
-weak_elements = password_strength_elements('Weak', '#fe8f59', ['• Numbers', '• Symbols'])
-so_so_elements = password_strength_elements('So-so', '#ffca0f', ['• Lower case', '• Numbers', '• Symbols'])
+very_weak_lower_elements = password_strength_elements('Very weak', '#ff2d55', ['✓ Lower case'])
+very_weak_upper_elements = password_strength_elements('Very weak', '#ff2d55', ['✓ Upper case'])
+very_weak_numbers_elements = password_strength_elements('Very weak', '#ff2d55', ['✓ Numbers'])
+very_weak_symbols_elements = password_strength_elements('Very weak', '#ff2d55', ['✓ Symbols'])
+weak_elements = password_strength_elements('Weak', '#fe8f59', ['✓ Numbers', '✓ Symbols'])
+okay_elements = password_strength_elements('Okay', '#ffca0f', ['✓ Lower case', '✓ Numbers', '✓ Symbols'])
 good_elements = password_strength_elements('Good', '#9ea85d',
-                                           ['• Lower case', '• Upper case', '• Numbers', '• Symbols'])
-great_elements = password_strength_elements('Great', '#4ebc60',
-                                            ['• Lower case', '• Upper case', '• Numbers', '• Symbols'])
+                                           ['✓ Lower case', '✓ Upper case', '✓ Numbers', '✓ Symbols'])
+strong_elements = password_strength_elements('Very strong', '#4ebc60',
+                                             ['✓ Lower case', '✓ Upper case', '✓ Numbers', '✓ Symbols'])
