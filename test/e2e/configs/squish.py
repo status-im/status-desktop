1c7738e7b248782f613ec28315ce626ebbea4cb1
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2025-01-15 17:46:05 +0300

    chore(@e2e): fix the port generation logic

--- test/e2e/configs/squish.py
+++ test/e2e/configs/squish.py
@@ -1,5 +1,5 @@
 import os
 
-AUT_PORT = 61500 + int(os.getenv('BUILD_NUMBER', 0))
-SERVER_PORT = 4322 + int(os.getenv('BUILD_NUMBER', 0))
+AUT_PORT = 61500 + int(os.getenv('EXECUTOR_NUMBER', 0))
+SERVER_PORT = 4322 + int(os.getenv('EXECUTOR_NUMBER', 0))
 CURSOR_ANIMATION = False
