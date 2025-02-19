dfe3d9955ba5ba7bf0bc26e671fadf9c58ad0e0f
Anastasiya <anastasija.ig@gmail.com>
2025-02-19 16:07:23 +0300

    chore(@e2e): change tests for new onboarding flows

--- test/e2e/configs/__init__.py
+++ test/e2e/configs/__init__.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 from os import path
 from scripts.utils.system_path import SystemPath
---
+++ test/e2e/gui/components/onboarding/login_users_list_popup.py
@@ -0,0 +1,28 @@
+import driver
+from gui.components.base_popup import BasePopup
+from gui.elements.button import Button
+from gui.elements.object import QObject
+from gui.objects_map import onboarding_names
+
+
+class OnboardingLoginUsersPopup(BasePopup):
+    def __init__(self):
+        super().__init__()
+        self.user_login_item = QObject(onboarding_names.userLoginItem)
+        self.create_profile_button = Button(onboarding_names.createProfileButton)
+        self.login_button = Button(onboarding_names.returningLoginButton)
+
+    def select_user_by_name(self, user_name):
+        raw_data = driver.findAllObjects(self.user_login_item.real_name)
+        if not raw_data:
+            raise ValueError(f"Can't find {user_name} in list of users as the list is empty")
+        for _user in raw_data:
+            name_label = str(QObject(_user).object.label)
+            if name_label == user_name:
+                try:
+                    QObject(_user).click()
+                    return self
+                except RuntimeError as e:
+                    raise RuntimeError(f'Could not click user with user name "{user_name}": {e}')
+        else:
+            raise ValueError(f'User "{user_name}" was not found')
