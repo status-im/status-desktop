d2d750df7de43ec3da5e9915d607aa53dff9b958
Siddarth Kumar <siddarthkay@gmail.com>
2025-02-25 17:13:26 +0530

    feat(keycard): use bundled pcscd and psclite

--- scripts/init_app_dir.sh
+++ scripts/init_app_dir.sh
@@ -9,7 +9,10 @@ mkdir -p \
   "${APP_DIR}/usr/lib" \
   "${APP_DIR}/usr/qml" \
   "${APP_DIR}/usr/i18n" \
-  "${APP_DIR}/usr/plugins/platforminputcontexts"
+  "${APP_DIR}/usr/plugins/platforminputcontexts" \
+  "${APP_DIR}/etc/reader.conf.d" \
+  "${APP_DIR}/usr/lib/pcsc/drivers" \
+  "${APP_DIR}/usr/bin"
 
 cp bin/nim_status_client "${APP_DIR}/usr/bin"
 cp bin/StatusQ/* "${APP_DIR}/usr/lib"
@@ -29,6 +32,20 @@ if [[ -z "${IN_NIX_SHELL}" ]]; then
 	cp -P /usr/lib/x86_64-linux-gnu/libgst* "${APP_DIR}/usr/lib/"
 	cp -r /usr/lib/x86_64-linux-gnu/gstreamer-1.0 "${APP_DIR}/usr/lib/"
 	cp -r /usr/lib/x86_64-linux-gnu/gstreamer1.0 "${APP_DIR}/usr/lib/"
+
+	echo "Bundling pcsc-lite 2.2.3..."
+	cp -L /usr/local/lib/x86_64-linux-gnu/libpcsclite.so* "${APP_DIR}/usr/lib/"
+	cp -L /usr/local/lib/x86_64-linux-gnu/libpcsclite_real.so* "${APP_DIR}/usr/lib/"
+	cp -L /usr/local/lib/x86_64-linux-gnu/pkgconfig/libpcsclite.pc "${APP_DIR}/usr/lib/"
+
+	chmod 755 "${APP_DIR}/usr/lib/libpcsclite.so"*
+	chmod 755 "${APP_DIR}/usr/lib/libpcsclite_real.so"*
+	chmod 755 "${APP_DIR}/usr/lib/libpcsclite.pc"
+
+	echo "Bundling pcscd..."
+	cp -L "/usr/local/sbin/pcscd"* "${APP_DIR}/usr/bin/"
+	chmod 755 "${APP_DIR}/usr/bin/pcscd"*
+
 else
 	mkdir -p "${APP_DIR}"/usr/lib/{gstreamer1.0,gstreamer-1.0,nss}
 	mkdir -p "${APP_DIR}"/usr/libexec
@@ -40,6 +57,8 @@ else
 	cp "${QTWEBENGINE_PATH}/libexec/QtWebEngineProcess" "${APP_DIR}/usr/libexec/QtWebEngineProcess"
 	cp "${QTWEBENGINE_PATH}"/resources/* "${APP_DIR}/usr/libexec/"
 	cp -r "${QTWEBENGINE_PATH}/translations/qtwebengine_locales" "${APP_DIR}/usr/libexec/"
+
+	#TODO: bundle pcsc-lite and pcscd in nix-shell
 
 	chmod -R u+w "${APP_DIR}/usr"
 fi
