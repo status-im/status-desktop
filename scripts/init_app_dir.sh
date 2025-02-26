bbc209aec563643e0099507eee792d9f2db18d53
Siddarth Kumar <siddarthkay@gmail.com>
2025-02-26 16:38:26 +0530

    keycard: link missing libpcsclite_real.so

--- scripts/init_app_dir.sh
+++ scripts/init_app_dir.sh
@@ -28,7 +28,9 @@ cp "${FCITX5_QT}" "${APP_DIR}/usr/plugins/platforminputcontexts/"
 
 echo "Bundling pcsc-lite 2.2.3..."
 cp -L /usr/local/lib/x86_64-linux-gnu/libpcsclite.so* "${APP_DIR}/usr/lib/"
+cp -L /usr/local/lib/x86_64-linux-gnu/libpcsclite_real.so* "${APP_DIR}/usr/lib/"
 chmod 755 "${APP_DIR}/usr/lib/libpcsclite.so"*
+chmod 755 "${APP_DIR}/usr/lib/libpcsclite_real.so"*
 
 cat > "${APP_DIR}/etc/reader.conf.d/000-bundled-pcsc.conf" << EOF
 # Use bundled pcsc-lite drivers
