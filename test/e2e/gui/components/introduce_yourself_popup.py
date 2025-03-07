c27718c0d317d4e872bffd3242a01e6d736e5550
Anastasiya <anastasija.ig@gmail.com>
2025-03-07 12:04:07 +0300

    chore(@e2e): handle introduce yourself popup

---
+++ test/e2e/gui/components/introduce_yourself_popup.py
@@ -0,0 +1,11 @@
+from gui.elements.button import Button
+from gui.elements.object import QObject
+from gui.objects_map import names
+
+
+class IntroduceYourselfPopup(QObject):
+    def __init__(self):
+        super().__init__(names.introduceYourselfEditProfileButton)
+
+        self.skip_button = Button(names.introduceYourselfSkipButton)
+        self.edit_profile_button = Button(names.introduceYourselfEditProfileButton)
--- test/e2e/gui/screens/community_portal.py
+++ test/e2e/gui/screens/community_portal.py
@@ -1,3 +1,5 @@
+import time
+
 import allure
 
 import driver
@@ -11,14 +13,15 @@ class CommunitiesPortal(QObject):
 
     def __init__(self):
         super().__init__(communities_names.mainWindow_communitiesPortalLayout_CommunitiesPortalLayout)
-        self._create_community_button = Button(communities_names.mainWindow_Create_New_Community_StatusButton)
+        self.create_community_from_portal_button = Button(communities_names.mainWindow_Create_New_Community_StatusButton)
 
     @allure.step('Open create community popup')
     def open_create_community_popup(self) -> CreateCommunityPopup:
-        self._create_community_button.click()
-        try:
-            assert driver.waitForObjectExists(CreateCommunitiesBanner().real_name, 5000), \
-                f'Create community banner is not present within 5 seconds'
-        except (Exception, AssertionError) as ex:
-            raise ex
-        return CreateCommunitiesBanner().open_create_community_popup()
+        for i in range(2):
+            self.create_community_from_portal_button.click()
+            time.sleep(0.1)
+            try:
+                return CreateCommunitiesBanner().wait_until_appears().open_create_community_popup()
+            except Exception:
+                pass
+        raise LookupError(f'Create Communities banner is not displayed')
