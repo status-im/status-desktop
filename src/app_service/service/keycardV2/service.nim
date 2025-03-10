3bfac0d990f7617ef60b3d59ce79b503a6a2ee84
Igor Sirotin <sirotin@status.im>
2025-03-10 19:54:40 +0000

    fix: try InitializeLibrary

--- src/app_service/service/keycardV2/service.nim
+++ src/app_service/service/keycardV2/service.nim
@@ -93,11 +93,12 @@ QtObject:
 
   proc init*(self: Service) =
     debug "KeycardServiceV2 init"
+    keycard_go.initializeLibrary()
     # Do not remove the sleep 700, this sleep prevents a crash on intel MacOS
     # with errors like bad flushGen 12 in prepareForSweep; sweepgen 0
     # More details: https://github.com/status-im/status-desktop/pull/15194
-    if status_const.IS_MACOS and status_const.IS_INTEL:
-      sleep 700
+    # if status_const.IS_MACOS and status_const.IS_INTEL:
+    #   sleep 700
     self.initializeRPC()
     self.asyncStart(status_const.KEYCARDPAIRINGDATAFILE)
     discard
