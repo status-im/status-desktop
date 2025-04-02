5d07a0d48ff79ef8412a905c2be1fbf4e24968ad
Siddarth Kumar <siddarthkay@gmail.com>
2025-04-02 22:35:57 +0400

    ci: fix clean script

--- scripts/clean-git.sh
+++ scripts/clean-git.sh
@@ -4,3 +4,5 @@ git clean -qfdx
 git submodule foreach --recursive git reset -q --hard
 git submodule foreach --recursive git clean -qfdx
 git submodule foreach --recursive rm -fr vendor
+# to get rid of lingering DOtherSide
+git clean -xdf vendor
