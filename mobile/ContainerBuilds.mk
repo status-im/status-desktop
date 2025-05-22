d65ecf29953a5bc1ecfb8f95ff9c3a21e76a72a7
Alex Jbanca <alexjb@status.im>
2025-05-22 23:38:27 +0300

    fix: Versioning using git describe fails on GH actions

--- mobile/ContainerBuilds.mk
+++ mobile/ContainerBuilds.mk
@@ -10,7 +10,7 @@ ARCH?=$(shell uname -m)
 $(TARGET): $(STATUS_DESKTOP_NIM_FILES) $(STATUS_DESKTOP_UI_FILES) $(STATUS_Q_FILES) $(STATUS_Q_UI_FILES) $(STATUS_GO_FILES) $(DOTHERSIDE_FILES) $(OPENSSL_FILES) $(QRCODEGEN_FILES) $(PCRE_FILES) $(WRAPPER_APP_FILES)
 	@echo "Building GitHub task"
 	act -j android-build --container-architecture linux/amd64 --env-file $(ROOT_DIR)/.github/workflows/.env-android-$(ARCH) linux/amd64 -r
-	@echo "Copying target"
+	@echo "Copying target to docker container $(shell docker ps -a --format '{{.Names}}' | grep Android-Build-APK)"
 	@mkdir -p $(BIN_PATH)
 	@docker cp $(shell docker ps -a --format '{{.Names}}' | grep Android-Build-APK):$(TARGET) $(BIN_PATH)
 
