9a0c75d192d4ba3eb8e9e192a6b4966e3f04c953
Siddarth Kumar <siddarthkay@gmail.com>
2025-06-03 15:19:13 +0530

    ci: upgrade openssl to 3.x

--- scripts/init_app_dir.sh
+++ scripts/init_app_dir.sh
@@ -46,6 +46,12 @@ if [[ -z "${IN_NIX_SHELL}" ]]; then
 	cp -L "/usr/local/sbin/pcscd"* "${APP_DIR}/usr/bin/"
 	chmod 755 "${APP_DIR}/usr/bin/pcscd"*
 
+	echo "Bundling OpenSSL 3.x..."
+	cp -L /usr/local/openssl3/lib64/libssl.so.3 "${APP_DIR}/usr/lib/"
+	cp -L /usr/local/openssl3/lib64/libcrypto.so.3 "${APP_DIR}/usr/lib/"
+	chmod 755 "${APP_DIR}/usr/lib/libssl.so.3"
+	chmod 755 "${APP_DIR}/usr/lib/libcrypto.so.3"
+
 else
 	mkdir -p "${APP_DIR}"/usr/lib/{gstreamer1.0,gstreamer-1.0,nss}
 	mkdir -p "${APP_DIR}"/usr/libexec
