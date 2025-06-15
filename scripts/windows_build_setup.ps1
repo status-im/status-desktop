9c7c09d2dd368d22aae3c28f81eb2c29f56ef3cd
Siddarth Kumar <siddarthkay@gmail.com>
2025-06-15 12:36:48 +0530

    ci: upgrade go to 1.23

--- scripts/windows_build_setup.ps1
+++ scripts/windows_build_setup.ps1
@@ -33,7 +33,7 @@ function Install-Dependencies {
     if (!(scoop bucket list | Where { $_.Name -eq "extras" })) {
         scoop bucket add extras
     }
-    scoop install --global go@1.22.10
+    scoop install --global go@1.23.10
     scoop install --global protobuf@3.20.0
     scoop install --global vcredist2022
     scoop install --global cmake@3.31.6
