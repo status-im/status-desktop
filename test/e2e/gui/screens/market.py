c511fb9b2f34482c8d3b92fa17dfefbca174317f
Mg <50769329+glitchminer@users.noreply.github.com>
2025-06-24 16:17:29 +0100

    test(Shell): Remove redundant wait_until_appears in market.py

--- test/e2e/gui/screens/market.py
+++ test/e2e/gui/screens/market.py
@@ -7,11 +7,5 @@ from gui.objects_map.names import statusDesktop_mainWindow
 class MarketScreen(QObject):
 
     def __init__(self):
-        # TODO: Using main window as container until we have specific market locators
+        # TODO: Add support for Market screen (https://github.com/status-im/status-desktop/issues/18235)
         super().__init__(statusDesktop_mainWindow)
-
-    @allure.step('Wait until Market screen appears')
-    def wait_until_appears(self, timeout_msec: int = 10000):
-        """Wait for Market screen to appear"""
-        super().wait_until_appears(timeout_msec)
-        return self 
