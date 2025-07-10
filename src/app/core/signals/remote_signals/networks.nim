69eaf8e2762acb60629c763ef6c029d0bf37cd7a
Dario Gabriel Lipicar <dario@status.im>
2025-07-10 11:29:24 -0300

    chore: remove deprecated networks connection status in favour of healthmanager

---
+++ src/app/core/signals/remote_signals/networks.nim
@@ -0,0 +1,12 @@
+import json, app_service/common/safe_json_serialization
+import base
+
+import backend/provider_status_types
+
+type NetworksBlockchainHealthChangedSignal* = ref object of Signal
+  fullStatus*: BlockchainFullStatus
+
+proc fromEvent*(T: type NetworksBlockchainHealthChangedSignal, event: JsonNode): NetworksBlockchainHealthChangedSignal =
+  result = NetworksBlockchainHealthChangedSignal()
+  result.signalType = SignalType.NetworksBlockchainHealthChanged
+  result.fullStatus = BlockchainFullStatus.fromJson(event["event"])
