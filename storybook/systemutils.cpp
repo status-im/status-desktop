00ca535bab551f671a98011b083505e8638c922f
Michał Cieślak <michalcieslak@status.im>
2025-07-24 17:19:53 +0200

    chore: Storybook externalized
    This PR removes storybook code from the repository and switches to use
    storybook from external repository. Storybook in the external repo is
    improved to be independent from it's original parent project and it's
    possible to use with arbitrary qt/qml project.
    
    Closes: #18430

--- storybook/systemutils.cpp
+++
@@ -1,9 +0,0 @@
-#include "systemutils.h"
-
-#include <QtGlobal>
-
-SystemUtils::SystemUtils(QObject *parent) : QObject(parent) {}
-
-QString SystemUtils::getEnvVar(const QString &varName) {
-    return qEnvironmentVariable(varName.toUtf8().constData(), {});
-}
