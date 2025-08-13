d2af110293ec64248bf76db7cf8e6de0ca2a4d6e
Siddarth Kumar <siddarthkay@gmail.com>
2025-08-13 18:03:48 +0530

    ci: nuke vendors in cleanup stage

--- scripts/clean-git.sh
+++ scripts/clean-git.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # Extracted from Makefile to avoid nimbus-build-system blocking use.
 git clean -qfdx
-git submodule foreach --recursive git reset -q --hard
-git submodule foreach --recursive git clean -qfdx
-# to get rid of lingering DOtherSide
-git clean -xdf vendor
-git clean -xdf mobile/vendor
+# nuke vendor, they're regenerated anyways
+rm -rf vendor
+rm -rf mobile/vendors
