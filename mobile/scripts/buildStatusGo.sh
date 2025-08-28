e841d464e48a0aa3275e32aef25e23d318fbbed3
Ivan Folgueira Bande <ivansete@status.im>
2025-08-28 17:05:21 +0200

    avoid make generate in Android

--- mobile/scripts/buildStatusGo.sh
+++ mobile/scripts/buildStatusGo.sh
@@ -32,9 +32,11 @@ if [[ "$OS" == "android" ]]; then
 	export ANDROID_TARGET=28
 	export ANDROID_NDK_HOME="/opt/android-sdk/ndk/27.2.12479018/"
 	make generate-sds-android V=3 SHELL=/bin/sh
+else
+	make generate V=3 SHELL=/bin/sh
 fi
 
-make generate V=3 SHELL=/bin/sh
+echo "AAA compiling jamon"
 
 mkdir -p build/bin/statusgo-lib
 go run cmd/library/main.go cmd/library/const.go > build/bin/statusgo-lib/main.go
