ed24ce995c3386f9752497b1dd329adbb41baa04
Anastasiya <anastasija.ig@gmail.com>
2025-09-09 14:32:12 +0300

    сhore(@e2e): fix test of editing links so its not changing occurrence property

--- test/e2e/gui/components/social_links_popup.py
+++ test/e2e/gui/components/social_links_popup.py
@@ -1,13 +1,13 @@
 import time
 import typing
+import copy
 
 import allure
 
 from gui.elements.button import Button
 from gui.elements.object import QObject
 from gui.elements.text_edit import TextEdit
-from .base_popup import BasePopup
-from ..objects_map import names
+from gui.objects_map import names
 
 
 class SocialLinksPopup(QObject):
@@ -24,21 +24,25 @@ class SocialLinksPopup(QObject):
         self._add_social_link_list_item.real_name['index'] = index
         return self._add_social_link_list_item
 
-    @allure.step('Get social link field')
-    def _get_text_field(self, occurrence: int) -> QObject:
-        key = 'occurrence'
-        if occurrence:
-            self._social_link_text_field.real_name[key] = occurrence + 1
-        else:
-            if key in self._social_link_text_field.real_name:
-                del self._social_link_text_field.real_name[key]
-        return self._social_link_text_field
+    @allure.step('Get social link field by index')
+    def _get_text_field_by_index(self, index: int) -> TextEdit:
+        """Get text field by index without modifying the original real_name"""
+        # Create a copy of the real_name to avoid modifying the original
+        text_field_real_name = copy.deepcopy(self._social_link_text_field.real_name)
+        
+        if index > 0:
+            text_field_real_name['occurrence'] = index + 1
+        
+        # Create a temporary TextEdit with the modified real_name
+        temp_text_field = TextEdit(text_field_real_name)
+        return temp_text_field
 
     @allure.step('Add link to link field')
     def add_link(self, index: int, links: typing.List[str]):
         self._get_list_item(index).click()
         time.sleep(0.5)
-        for occurrence, link in enumerate(links):
-            self._get_text_field(occurrence).text = link
+        for field_index, link in enumerate(links):
+            text_field = self._get_text_field_by_index(field_index)
+            text_field.text = link
         self._add_button.click()
         self.wait_until_hidden()
