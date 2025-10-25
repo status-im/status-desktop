3529b67418a7931cbd760ece6d6be085cad8d006
Igor Sirotin <sirotin@status.im>
2025-10-25 12:03:18 +0100

    fix: scripts init_app_dir

--- scripts/init_app_dir.sh
+++ scripts/init_app_dir.sh
@@ -24,7 +24,7 @@ cp vendor/status-go/build/bin/libstatus.so "${APP_DIR}/usr/lib/"
 cp vendor/status-go/build/bin/libstatus.so.0 "${APP_DIR}/usr/lib/"
 cp "${STATUSKEYCARDGO}" "${APP_DIR}/usr/lib/"
 if [ "${USE_NWAKU}" = "true" ]; then
-  cp vendor/status-go/vendor/github.com/waku-org/waku-go-bindings/third_party/nwaku/build/libwaku.so "${APP_DIR}/usr/lib/"
+  cp ${NWAKU_SOURCE_DIR}/build/libwaku.so "${APP_DIR}/usr/lib/"
 fi
 cp "${FCITX5_QT}" "${APP_DIR}/usr/plugins/platforminputcontexts/"
 
