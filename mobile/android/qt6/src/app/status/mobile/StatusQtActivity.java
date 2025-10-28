ba3bd1ba0401c5933c7ba8ac0760fc08f1b5691b
Alex Jbanca <47811206+alexjba@users.noreply.github.com>
2025-10-28 14:02:55 +0200

    fix: Android16 keyboard not showing up on tap (#19158)
    * fix: Android16 keyboard not showing up on tap
    
    Simulate a second tap when focusing a text edit. This will ensure the keyboard is triggered on Android 16.
    This code is guarded by android version and will only be used on android 16
    
    reference: https://bugreports.qt.io/browse/QTBUG-140897
    
    * fix: Don't apply the keyboard hack on android 15

--- mobile/android/qt6/src/app/status/mobile/StatusQtActivity.java
+++ mobile/android/qt6/src/app/status/mobile/StatusQtActivity.java
@@ -8,14 +8,35 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class StatusQtActivity extends QtActivity {
     private static final AtomicBoolean splashShouldHide = new AtomicBoolean(false);
+    
+    // QTBUG-140897: Android 16 keyboard workaround
+    // Remove this line when Qt 6.10+ fixes the issue, and delete Android16KeyboardWorkaround.java
+    private Android16KeyboardWorkaround mKeyboardWorkaround;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         if (Build.VERSION.SDK_INT >= 31) { // Android 12+
-                SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
-                splashScreen.setKeepOnScreenCondition(() -> !splashShouldHide.get());
+            SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+            splashScreen.setKeepOnScreenCondition(() -> !splashShouldHide.get());
         }
+        
+        // QTBUG-140897: Install Android 16 keyboard workaround
+        // Remove this line when Qt 6.10+ fixes the issue
+        mKeyboardWorkaround = Android16KeyboardWorkaround.install(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // QTBUG-140897: Cleanup workaround resources
+        // Remove this when Qt 6.10+ fixes the issue
+        if (mKeyboardWorkaround != null) {
+            mKeyboardWorkaround.cleanup();
+            mKeyboardWorkaround = null;
+        }
+        
+        super.onDestroy();
     }
 
     // Called from Qt via JNI when main window is visible
