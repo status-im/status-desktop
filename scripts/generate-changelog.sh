c7c3d589e9b5b6fbf40dd6d470956abf4ddaa326
Siddarth Kumar <siddarthkay@gmail.com>
2025-10-31 15:57:35 +0400

    ci: fix branch name var

--- scripts/generate-changelog.sh
+++ scripts/generate-changelog.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+BRANCH_NAME="${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}"
+
 if [[ -n "${CHANGE_ID:-}" ]]; then
   # PR build
   SHORT_COMMIT="${GIT_COMMIT:0:7}"
@@ -8,7 +10,7 @@ if [[ -n "${CHANGE_ID:-}" ]]; then
   echo "Build: #${BUILD_NUMBER}"
   echo ""
   echo "View PR: https://github.com/status-im/status-desktop/pull/${CHANGE_ID}"
-elif [[ "${BRANCH_NAME:-}" == release* ]]; then
+elif [[ "${BRANCH_NAME}" == release* ]]; then
   # Release branch build
   echo "Release ${VERSION}"
   echo "Build: #${BUILD_NUMBER}"
