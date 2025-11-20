05fa413dc67ca0d238d6c856bbb11e29fd4f03dc
Michał Cieślak <michalcieslak@status.im>
2025-11-20 11:19:11 +0100

    Theme: font size handling added

--- ui/StatusQ/include/StatusQ/theme.h
+++ ui/StatusQ/include/StatusQ/theme.h
@@ -2,7 +2,7 @@
 
 #include "StatusQ/themepalette.h"
 
-#include <QObject>
+#include <QJSValue>
 #include <QQmlEngine>
 #include <QQuickAttachedPropertyPropagator>
 
@@ -10,19 +10,46 @@ class Theme : public QQuickAttachedPropertyPropagator
 {
     Q_OBJECT
 
-    Q_PROPERTY(qreal padding READ padding WRITE setPadding RESET resetPadding NOTIFY paddingChanged)
+    Q_PROPERTY(qreal padding READ padding WRITE setPadding
+                   RESET resetPadding NOTIFY paddingChanged)
     Q_PROPERTY(qreal xlPadding READ xlPadding NOTIFY paddingChanged)
     Q_PROPERTY(qreal bigPadding READ bigPadding NOTIFY paddingChanged)
     Q_PROPERTY(qreal halfPadding READ halfPadding NOTIFY paddingChanged)
     Q_PROPERTY(qreal smallPadding READ smallPadding NOTIFY paddingChanged)
     Q_PROPERTY(qreal radius READ radius CONSTANT)
 
-    Q_PROPERTY(bool explicitPadding READ explicitPadding NOTIFY explicitPaddingChanged)
+    Q_PROPERTY(bool explicitPadding READ explicitPadding
+                   NOTIFY explicitPaddingChanged)
 
-    Q_PROPERTY(Style style READ style WRITE setStyle RESET resetStyle NOTIFY styleChanged)
-    Q_PROPERTY(const ThemePalette* palette READ palette NOTIFY styleChanged)
+    Q_PROPERTY(Style style READ style WRITE setStyle RESET resetStyle
+                   NOTIFY styleChanged)
+    Q_PROPERTY(const ThemePalette* palette READ palette
+                   NOTIFY styleChanged)
 
-    Q_PROPERTY(bool explicitStyle READ explicitStyle NOTIFY explicitStyleChanged)
+    Q_PROPERTY(bool explicitStyle READ explicitStyle
+                   NOTIFY explicitStyleChanged)
+
+    Q_PROPERTY(int fontSizeOffset READ fontSizeOffset WRITE setFontSizeOffset
+                   RESET resetFontSizeOffset NOTIFY fontSizeOffsetChanged)
+
+    Q_PROPERTY(int secondaryAdditionalTextSize
+                   READ secondaryAdditionalTextSize
+                   NOTIFY fontSizeOffsetChanged)
+    Q_PROPERTY(int primaryTextFontSize READ primaryTextFontSize
+                   NOTIFY fontSizeOffsetChanged)
+    Q_PROPERTY(int secondaryTextFontSize READ secondaryTextFontSize
+                   NOTIFY fontSizeOffsetChanged)
+    Q_PROPERTY(int additionalTextSize READ additionalTextSize
+                   NOTIFY fontSizeOffsetChanged)
+    Q_PROPERTY(int tertiaryTextFontSize READ tertiaryTextFontSize
+                   NOTIFY fontSizeOffsetChanged)
+    Q_PROPERTY(int asideTextFontSize READ asideTextFontSize
+                   NOTIFY fontSizeOffsetChanged)
+
+    Q_PROPERTY(bool explicitFontSizeOffset READ explicitFontSizeOffset
+                   NOTIFY explicitFontSizeOffsetChanged)
+
+    Q_PROPERTY(QJSValue fontSize READ fontSize NOTIFY fontSizeOffsetChanged)
 
     enum class Style {
         Light,
@@ -34,6 +61,7 @@ class Theme : public QQuickAttachedPropertyPropagator
 public:
     explicit Theme(QObject *parent = nullptr);
 
+    // paddings
     qreal padding() const;
     qreal xlPadding() const;
     qreal bigPadding() const;
@@ -43,35 +71,57 @@ public:
 
     void setPadding(qreal padding);
     void resetPadding();
-
-    void inheritPadding(qreal padding);
-    void propagatePadding();
-
     bool explicitPadding() const;
 
+    // light/dark style
     Style style() const;
-    void setStyle(Style style);
-    void resetStyle();
-
-    void inheritStyle(Style style);
-    void propagateStyle();
-
-    bool explicitStyle() const;
-
     const ThemePalette* palette() const;
 
+    void setStyle(Style style);
+    void resetStyle();
+    bool explicitStyle() const;
+
+    // font size
+    int fontSizeOffset() const;
+    int secondaryAdditionalTextSize() const;
+    int primaryTextFontSize() const;
+    int secondaryTextFontSize() const;
+    int additionalTextSize() const;
+    int tertiaryTextFontSize() const;
+    int asideTextFontSize() const;
+
+    QJSValue fontSize() const;
+
+    void setFontSizeOffset(int fontSizeOffset);
+    void resetFontSizeOffset();
+    bool explicitFontSizeOffset() const;
+
+    // top level object access
     Q_INVOKABLE Theme* rootTheme();
 
+    // attached object instantiation
     static Theme *qmlAttachedProperties(QObject *object);
 
 signals:
     void paddingChanged();
     void explicitPaddingChanged();
 
-    void explicitStyleChanged();
     void styleChanged();
+    void explicitStyleChanged();
+
+    void fontSizeOffsetChanged();
+    void explicitFontSizeOffsetChanged();
 
 protected:
+    void inheritPadding(qreal padding);
+    void propagatePadding();
+
+    void inheritStyle(Style style);
+    void propagateStyle();
+
+    void inheritFontSizeOffset(int fontSizeOffset);
+    void propagateFontSizeOffset();
+
     void attachedParentChange(
         QQuickAttachedPropertyPropagator *newParent,
         QQuickAttachedPropertyPropagator *oldParent) override;
@@ -82,6 +132,10 @@ private:
 
     bool m_explicitStyle = false;
     Style m_style = Style::Light;
+
+    bool m_explicitFontSizeOffset = false;
+    int m_fontSizeOffset = 0;
+    mutable QJSValue m_fontSizeFn;
 };
 
 QML_DECLARE_TYPEINFO(Theme, QML_HAS_ATTACHED_PROPERTIES)
