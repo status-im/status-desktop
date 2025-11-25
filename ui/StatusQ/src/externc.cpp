3bdfeb2dd5c778a37876b7e1722b8e43a47f8e89
Alex Jbanca <alexjb@status.im>
2025-11-25 12:22:01 +0200

    feat: Add dynamic scaling for mobile devices
    I've been taking as a reference a Samsung S21 FE with 0.8 scaling. It has a rough dp value of 360.
    The scaling is clamped between 0.8 and 1.0.

--- ui/StatusQ/src/externc.cpp
+++ ui/StatusQ/src/externc.cpp
@@ -1,11 +1,16 @@
 #include <QtGlobal>
 
 #include <StatusQ/typesregistration.h>
+#include <MobileUI>
 
 extern "C" {
 
 Q_DECL_EXPORT void statusq_registerQmlTypes() {
     registerStatusQTypes();
+}
+
+Q_DECL_EXPORT float statusq_getMobileUIScaleFactor(float baseWidth, float baseDpi, float baseScale) {
+    return MobileUI::getSmartScaleFactor(baseWidth, baseDpi, baseScale);
 }
 
 } // extern "C"
