e35c00c2fd83f3cd2468caa462d080497f6e3a1f
Michał Cieślak <michalcieslak@status.im>
2025-12-11 16:30:33 +0100

    Theme: add comments with hex color values
    Colors defined via QColor(int, int, int) are not search-friendly
    (pairing color from Figma to name in code). To solve that, comments
    witch standard hex notation are added.

--- ui/StatusQ/include/StatusQ/statuscolors.h
+++ ui/StatusQ/include/StatusQ/statuscolors.h
@@ -100,86 +100,86 @@ public:
     Q_INVOKABLE static QColor getColor(const QString& name, qreal alpha = -1);
     Q_INVOKABLE static QColor alphaColor(const QColor& color, qreal alpha);
 
-    static constexpr QColor black = QColor(0x00, 0x00, 0x00);
-    static constexpr QColor white = QColor(0xFF, 0xFF, 0xFF);
+    static constexpr QColor black = QColor(0x00, 0x00, 0x00); // #000000
+    static constexpr QColor white = QColor(0xFF, 0xFF, 0xFF); // #FFFFFF
     static constexpr QColor transparent = QColor(0x00, 0x00, 0x00, 0x00);
 
-    static constexpr QColor blue = QColor(0x43, 0x60, 0xDF);
-    static constexpr QColor blue2 = QColor(0x29, 0x46, 0xC4);
-    static constexpr QColor blue3 = QColor(0x88, 0xB0, 0xFF);
-    static constexpr QColor blue4 = QColor(0x86, 0x9E, 0xFF);
-    static constexpr QColor blue5 = QColor(0xAA, 0xC6, 0xFF);
-    static constexpr QColor blue6 = QColor(0xEC, 0xEF, 0xFC);
-    static constexpr QColor blue7 = QColor(0x09, 0x10, 0x1C);
-    static constexpr QColor blue8 = QColor(0x6B, 0x6F, 0x76);
+    static constexpr QColor blue = QColor(0x43, 0x60, 0xDF); // #4360DF
+    static constexpr QColor blue2 = QColor(0x29, 0x46, 0xC4); // #2946C4
+    static constexpr QColor blue3 = QColor(0x88, 0xB0, 0xFF); // #88B0FF
+    static constexpr QColor blue4 = QColor(0x86, 0x9E, 0xFF); // #869EFF
+    static constexpr QColor blue5 = QColor(0xAA, 0xC6, 0xFF); // #AAC6FF
+    static constexpr QColor blue6 = QColor(0xEC, 0xEF, 0xFC); // #ECEFFC
+    static constexpr QColor blue7 = QColor(0x09, 0x10, 0x1C); // #09101C
+    static constexpr QColor blue8 = QColor(0x6B, 0x6F, 0x76); // #6B6F76
 
-    static constexpr QColor brown = QColor(0x8B, 0x31, 0x31);
-    static constexpr QColor brown2 = QColor(0x9B, 0x83, 0x2F);
-    static constexpr QColor brown3 = QColor(0xAD, 0x43, 0x43);
+    static constexpr QColor brown = QColor(0x8B, 0x31, 0x31); // #8B3131
+    static constexpr QColor brown2 = QColor(0x9B, 0x83, 0x2F); // #9B832F
+    static constexpr QColor brown3 = QColor(0xAD, 0x43, 0x43); // #AD4343
 
-    static constexpr QColor cyan = QColor(0x51, 0xD0, 0xF0);
+    static constexpr QColor cyan = QColor(0x51, 0xD0, 0xF0); // #51D0F0
 
-    static constexpr QColor graphite = QColor(0x21, 0x21, 0x21);
-    static constexpr QColor graphite2 = QColor(0x25, 0x25, 0x25);
-    static constexpr QColor graphite3 = QColor(0x2C, 0x2C, 0x2C);
-    static constexpr QColor graphite4 = QColor(0x37, 0x37, 0x37);
-    static constexpr QColor graphite5 = QColor(0x90, 0x90, 0x90);
+    static constexpr QColor graphite = QColor(0x21, 0x21, 0x21); // #212121
+    static constexpr QColor graphite2 = QColor(0x25, 0x25, 0x25); // #252525
+    static constexpr QColor graphite3 = QColor(0x2C, 0x2C, 0x2C); // #2C2C2C
+    static constexpr QColor graphite4 = QColor(0x37, 0x37, 0x37); // #373737
+    static constexpr QColor graphite5 = QColor(0x90, 0x90, 0x90); // #909090
 
-    static constexpr QColor green = QColor(0x4E, 0xBC, 0x60);
-    static constexpr QColor green2 = QColor(0x7C, 0xDA, 0x00);
-    static constexpr QColor green3 = QColor(0x60, 0xC3, 0x70);
-    static constexpr QColor green4 = QColor(0x93, 0xDB, 0x33);
-    static constexpr QColor green5 = QColor(0x9E, 0xA8, 0x5D);
-    static constexpr QColor green6 = QColor(0xAF, 0xB5, 0x51);
+    static constexpr QColor green = QColor(0x4E, 0xBC, 0x60); // #4EBC60
+    static constexpr QColor green2 = QColor(0x7C, 0xDA, 0x00); // #7CDA00
+    static constexpr QColor green3 = QColor(0x60, 0xC3, 0x70); // #60C370
+    static constexpr QColor green4 = QColor(0x93, 0xDB, 0x33); // #93DB33
+    static constexpr QColor green5 = QColor(0x9E, 0xA8, 0x5D); // #9EA85D
+    static constexpr QColor green6 = QColor(0xAF, 0xB5, 0x51); // #AFB551
 
-    static constexpr QColor grey = QColor(0xF0, 0xF2, 0xF5);
-    static constexpr QColor grey2 = QColor(0xF6, 0xF8, 0xFA);
-    static constexpr QColor grey3 = QColor(0xE9, 0xED, 0xF1);
-    static constexpr QColor grey4 = QColor(0xEE, 0xF2, 0xF5);
-    static constexpr QColor grey5 = QColor(0x93, 0x9B, 0xA1);
+    static constexpr QColor grey = QColor(0xF0, 0xF2, 0xF5); // #F0F2F5
+    static constexpr QColor grey2 = QColor(0xF6, 0xF8, 0xFA); // #F6F8FA
+    static constexpr QColor grey3 = QColor(0xE9, 0xED, 0xF1); // #E9EDF1
+    static constexpr QColor grey4 = QColor(0xEE, 0xF2, 0xF5); // #EEF2F5
+    static constexpr QColor grey5 = QColor(0x93, 0x9B, 0xA1); // #939BA1
 
-    static constexpr QColor moss = QColor(0x26, 0xA6, 0x9A);
-    static constexpr QColor moss2 = QColor(0x10, 0xA8, 0x8E);
+    static constexpr QColor moss = QColor(0x26, 0xA6, 0x9A); // #26A69A
+    static constexpr QColor moss2 = QColor(0x10, 0xA8, 0x8E); // #10A88E
 
-    static constexpr QColor orange = QColor(0xFE, 0x8F, 0x59);
-    static constexpr QColor orange2 = QColor(0xFF, 0x9F, 0x0F);
-    static constexpr QColor orange3 = QColor(0xFF, 0xA6, 0x7B);
-    static constexpr QColor orange4 = QColor(0xFE, 0x8F, 0x59);
+    static constexpr QColor orange = QColor(0xFE, 0x8F, 0x59); // #FE8F59
+    static constexpr QColor orange2 = QColor(0xFF, 0x9F, 0x0F); // #FF9F0F
+    static constexpr QColor orange3 = QColor(0xFF, 0xA6, 0x7B); // #FFA67B
+    static constexpr QColor orange4 = QColor(0xFE, 0x8F, 0x59); // #FE8F59
 
-    static constexpr QColor warning_orange = QColor(0xF6, 0x79, 0x3C);
+    static constexpr QColor warning_orange = QColor(0xF6, 0x79, 0x3C); // #F6793C
 
-    static constexpr QColor purple = QColor(0x88, 0x7A, 0xF9);
+    static constexpr QColor purple = QColor(0x88, 0x7A, 0xF9); // #887AF9
 
-    static constexpr QColor red = QColor(0xFF, 0x2D, 0x55);
-    static constexpr QColor red2 = QColor(0xFA, 0x65, 0x65);
-    static constexpr QColor red3 = QColor(0xFF, 0x5C, 0x7B);
+    static constexpr QColor red = QColor(0xFF, 0x2D, 0x55); // #FF2D55
+    static constexpr QColor red2 = QColor(0xFA, 0x65, 0x65); // #FA6565
+    static constexpr QColor red3 = QColor(0xFF, 0x5C, 0x7B); // #FF5C7B
 
-    static constexpr QColor turquoise = QColor(0x0D, 0xA4, 0xC9);
-    static constexpr QColor turquoise2 = QColor(0x07, 0xBC, 0xE9);
-    static constexpr QColor turquoise3 = QColor(0x7B, 0xE5, 0xFF);
-    static constexpr QColor turquoise4 = QColor(0x0D, 0xA4, 0xC9);
+    static constexpr QColor turquoise = QColor(0x0D, 0xA4, 0xC9); // #0DA4C9
+    static constexpr QColor turquoise2 = QColor(0x07, 0xBC, 0xE9); // #07BCE9
+    static constexpr QColor turquoise3 = QColor(0x7B, 0xE5, 0xFF); // #7BE5FF
+    static constexpr QColor turquoise4 = QColor(0x0D, 0xA4, 0xC9); // #0DA4C9
 
-    static constexpr QColor violet = QColor(0xD3, 0x7E, 0xF4);
+    static constexpr QColor violet = QColor(0xD3, 0x7E, 0xF4); // #D37EF4
 
-    static constexpr QColor yellow = QColor(0xFF, 0xCA, 0x0F);
-    static constexpr QColor yellow2 = QColor(0xEA, 0xD2, 0x7B);
+    static constexpr QColor yellow = QColor(0xFF, 0xCA, 0x0F); // #FFCA0F
+    static constexpr QColor yellow2 = QColor(0xEA, 0xD2, 0x7B); // #EAD27B
 
-    static constexpr QColor blueHijab = QColor(0xCD, 0xF2, 0xFB);
+    static constexpr QColor blueHijab = QColor(0xCD, 0xF2, 0xFB); // #CDF2FB
 
-    static constexpr QColor lightPattensBlue = QColor(0xD7, 0xDE, 0xE4);
+    static constexpr QColor lightPattensBlue = QColor(0xD7, 0xDE, 0xE4); // #D7DEE4
 
-    static constexpr QColor blackHovered = QColor(0x1D, 0x23, 0x2E);
-    static constexpr QColor blueHovered = QColor(0x36, 0x4D, 0xB2);
-    static constexpr QColor purpleHovered = QColor(0x6D, 0x62, 0xC7);
-    static constexpr QColor cyanHovered = QColor(0x41, 0xA6, 0xC0);
-    static constexpr QColor violetHovered = QColor(0xA9, 0x65, 0xC3);
-    static constexpr QColor redHovered = QColor(0xC8, 0x51, 0x51);
-    static constexpr QColor yellowHovered = QColor(0xCC, 0xA2, 0x0C);
-    static constexpr QColor greenHovered = QColor(0x63, 0xAE, 0x00);
-    static constexpr QColor mossHovered = QColor(0x1E, 0x85, 0x7B);
-    static constexpr QColor brownHovered = QColor(0x6F, 0x27, 0x27);
-    static constexpr QColor brown2Hovered = QColor(0x7C, 0x69, 0x26);
+    static constexpr QColor blackHovered = QColor(0x1D, 0x23, 0x2E); // #1D232E
+    static constexpr QColor blueHovered = QColor(0x36, 0x4D, 0xB2); // #364DB2
+    static constexpr QColor purpleHovered = QColor(0x6D, 0x62, 0xC7); // #6D62C7
+    static constexpr QColor cyanHovered = QColor(0x41, 0xA6, 0xC0); // #41A6C0
+    static constexpr QColor violetHovered = QColor(0xA9, 0x65, 0xC3); // #A965C3
+    static constexpr QColor redHovered = QColor(0xC8, 0x51, 0x51); // #C85151
+    static constexpr QColor yellowHovered = QColor(0xCC, 0xA2, 0x0C); // #CCA20C
+    static constexpr QColor greenHovered = QColor(0x63, 0xAE, 0x00); // #63AE00
+    static constexpr QColor mossHovered = QColor(0x1E, 0x85, 0x7B); // #1E857B
+    static constexpr QColor brownHovered = QColor(0x6F, 0x27, 0x27); // #6F2727
+    static constexpr QColor brown2Hovered = QColor(0x7C, 0x69, 0x26); // #7C6926
 
-    static constexpr QColor lightDesktopBlue10 = QColor(0xEC, 0xEF, 0xFB);
-    static constexpr QColor darkDesktopBlue10 = QColor(0x27, 0x32, 0x51);
+    static constexpr QColor lightDesktopBlue10 = QColor(0xEC, 0xEF, 0xFB); // #ECEFFB
+    static constexpr QColor darkDesktopBlue10 = QColor(0x27, 0x32, 0x51); // #273251
 };
